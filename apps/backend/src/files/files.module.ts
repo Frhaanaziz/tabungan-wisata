49ccdf8a1d4df0fe5ab9daf7dc7fa2cd1c382f0b
Farhan Aziz <farhan@aththariq.com>
2024-01-22 10:58:59 +0700

    Refactor files.module.ts to import UtilsModule

--- apps/backend/src/files/files.module.ts
+++ apps/backend/src/files/files.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
-import { v2 as cloudinary } from 'cloudinary';
+import { UtilsModule } from 'src/utils/utils.module';
 
 @Module({
+  imports: [UtilsModule],
   controllers: [FilesController],
-  providers: [
-    FilesService,
-    {
-      provide: 'CLOUDINARY', // 👈
-      useFactory: () => {
-        return cloudinary.config({
-          cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-          api_key: process.env.CLOUDINARY_API_KEY,
-          api_secret: process.env.CLOUDINARY_API_SECRET,
-        });
-      },
-    },
-  ],
-  exports: [FilesService],
+  providers: [FilesService],
 })
 export class FilesModule {}
