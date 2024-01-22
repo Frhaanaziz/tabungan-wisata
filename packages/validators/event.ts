aa5cb7c292a375ee7d6a299c2a0998909d888e66
Farhan Aziz <farhan@aththariq.com>
2024-01-22 10:59:15 +0700

    Fix quotes and add image validation to event schema

--- packages/validators/event.ts
+++ packages/validators/event.ts
@@ -1,18 +1,21 @@
-import { z } from "zod";
+import { z } from 'zod';
+import { fileSchema } from './file';
 export const eventSchema = z.object({
   id: z.string().cuid(),
   name: z
     .string()
-    .min(1, { message: "Name must have at least 1 character" })
+    .min(1, { message: 'Name must have at least 1 character' })
     .max(190),
   description: z
     .string()
-    .min(1, { message: "Description must have at least 1 character" })
-    .max(190),
+    .min(1, { message: 'Description must have at least 1 character' }),
   startDate: z.date(),
   endDate: z.date(),
   cost: z.coerce.number().min(0),
-  schoolId: z.string().cuid({ message: "Please select a school" }),
+  schoolId: z.string().cuid({ message: 'Please select a school' }),
+  images: z
+    .array(fileSchema.omit({ id: true }))
+    .min(1, { message: 'Please upload at least 1 image' }),
   // registrations: z.array(eventRegistrationSchema).optional(),
   // school: schoolSchema.optional(),
 });
