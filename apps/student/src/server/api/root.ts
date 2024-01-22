60fd8cc377cf7ece13ee0850021f0908c034870c
Farhan Aziz <farhan@aththariq.com>
2024-01-22 18:11:26 +0700

    Add event router to appRouter

--- apps/student/src/server/api/root.ts
+++ apps/student/src/server/api/root.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { userRouter } from "./routers/users";
 import { authRouter } from "./routers/auth";
+import { eventRouter } from "./routers/events";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
   auth: authRouter,
+  event: eventRouter,
 });
 
 // export type definition of API
