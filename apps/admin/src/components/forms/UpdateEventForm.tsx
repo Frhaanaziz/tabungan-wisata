5bca77682d910c67c04fd46db7bcfb68d9d93bdd
Farhan Aziz <farhan@aththariq.com>
2024-01-22 10:58:42 +0700

    Fix bug in login functionality
    Refactor code to improve performance
    
    Add new feature for user authentication
    
    Update documentation for API endpoints
    
    Fix formatting issues in code
    
    Update error handling in login process
    
    Remove unused variables
    
    Optimize database queries
    
    Add unit tests for new functionality
    
    Fix security vulnerability in authentication process

--- apps/admin/src/components/forms/UpdateEventForm.tsx
+++ apps/admin/src/components/forms/UpdateEventForm.tsx
@@ -18,7 +18,6 @@ import {
   PopoverTrigger,
 } from "@ui/components/popover";
 import { Input } from "@ui/components/input";
-import { Textarea } from "@ui/components/textarea";
 import SubmitButton from "../SubmitButton";
 import { api } from "@/trpc/react";
 import { toast } from "sonner";
@@ -29,16 +28,27 @@ import FormFieldWrapper from "./FormFieldWrapper";
 import { format } from "date-fns";
 import { cn } from "@repo/utils";
 import { Calendar } from "@ui/components/calendar";
+import { MultiFileDropzoneField } from "./MultiFileDropzoneField";
+import React from "react";
+import RichTextEditor from "../RichTextEditor";
 
 type UpdateEventType = z.infer<typeof updateEventSchema>;
 
-const UpdateEventForm = ({ event }: { event: UpdateEventType }) => {
+const UpdateEventForm = ({
+  event,
+  setModalOpen,
+}: {
+  event: UpdateEventType;
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const [isUploadingImage, setIsUploadingImage] = React.useState(false);
   const utils = api.useUtils();
 
   const defaultValues: UpdateEventType = {
     ...event,
     startDate: new Date(event.startDate),
     endDate: new Date(event.endDate),
+    images: [],
   };
 
   const form = useForm<UpdateEventType>({
@@ -46,14 +56,16 @@ const UpdateEventForm = ({ event }: { event: UpdateEventType }) => {
     defaultValues,
   });
 
-  const { handleSubmit, control } = form;
+  const { handleSubmit, control, formState } = form;
 
   const { mutate, isLoading } = api.event.update.useMutation({
     onSuccess: async () => {
+      setModalOpen(false);
       toast.success("Event updated successfully");
       await utils.event.invalidate();
     },
     onError: (err) => {
+      setModalOpen(false);
       toast.error(err.message);
     },
   });
@@ -64,142 +76,171 @@ const UpdateEventForm = ({ event }: { event: UpdateEventType }) => {
         onSubmit={handleSubmit((value) => mutate(value))}
         className="space-y-8"
       >
-        <FormField
-          control={control}
-          name="name"
-          disabled={isLoading}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input {...field} placeholder="Enter event name" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={control}
-          name="description"
-          disabled={isLoading}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Textarea {...field} placeholder="Enter event description" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={control}
-          name="cost"
-          disabled={isLoading}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Cost</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  type="number"
-                  min={0}
-                  placeholder="Enter event name"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormFieldWrapper>
+        <div className="space-y-5">
           <FormField
-            control={form.control}
-            name="startDate"
+            control={control}
+            name="name"
+            disabled={isLoading}
             render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Event start Date</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[240px] pl-3 text-left font-normal",
-                          !field.value && "text-muted-foreground",
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, "PPP")
-                        ) : (
-                          <span>Pick a date</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={field.value}
-                      onSelect={field.onChange}
-                      // disabled={(date) =>
-                      //   date > new Date() || date < new Date("1900-01-01")
-                      // }
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Enter event name" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={control}
+            name="description"
+            disabled={isLoading}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <RichTextEditor
+                    {...field}
+                    {...formState}
+                    isLoading={isLoading}
+                  />
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
           <FormField
-            control={form.control}
-            name="endDate"
+            control={control}
+            name="cost"
+            disabled={isLoading}
             render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Event end date</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[240px] pl-3 text-left font-normal",
-                          !field.value && "text-muted-foreground",
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, "PPP")
-                        ) : (
-                          <span>Pick a date</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={field.value}
-                      onSelect={field.onChange}
-                      // disabled={(date) =>
-                      //   date > new Date() || date < new Date("1900-01-01")
-                      // }
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
+              <FormItem>
+                <FormLabel>Cost</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    type="number"
+                    min={0}
+                    placeholder="Enter event name"
+                  />
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-        </FormFieldWrapper>
 
-        <SubmitButton isSubmitting={isLoading} />
+          <FormFieldWrapper>
+            <FormField
+              control={form.control}
+              name="startDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Event start Date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        // disabled={(date) =>
+                        //   date > new Date() || date < new Date("1900-01-01")
+                        // }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="endDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Event end date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        // disabled={(date) =>
+                        //   date > new Date() || date < new Date("1900-01-01")
+                        // }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FormFieldWrapper>
+
+          <FormField
+            control={control}
+            name="images"
+            disabled={isLoading}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Images</FormLabel>
+                <FormControl>
+                  <MultiFileDropzoneField
+                    {...field}
+                    {...formState}
+                    category="event"
+                    isSubmitting={isLoading}
+                    isUploading={(isUploading) =>
+                      setIsUploadingImage(isUploading)
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <SubmitButton isSubmitting={isLoading || isUploadingImage} />
       </form>
     </Form>
   );
