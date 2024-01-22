b46941f99fc12d16f9aec748e6f380db1335950c
Farhan Aziz <farhan@aththariq.com>
2024-01-22 18:11:46 +0700

    Refactor event schema and validators

--- packages/validators/event.ts
+++ packages/validators/event.ts
@@ -9,19 +9,24 @@ export const eventSchema = z.object({
   description: z
     .string()
     .min(1, { message: 'Description must have at least 1 character' }),
-  startDate: z.date(),
-  endDate: z.date(),
+  startDate: z.coerce.date(),
+  endDate: z.coerce.date(),
   cost: z.coerce.number().min(0),
   schoolId: z.string().cuid({ message: 'Please select a school' }),
   images: z
-    .array(fileSchema.omit({ id: true }))
+    // .array(fileSchema.omit({ id: true }))
+    .array(fileSchema)
     .min(1, { message: 'Please upload at least 1 image' }),
   // registrations: z.array(eventRegistrationSchema).optional(),
   // school: schoolSchema.optional(),
 });
 
-export const addEventSchema = eventSchema.omit({ id: true });
-
-export const updateEventSchema = eventSchema.omit({
-  schoolId: true,
+export const addEventSchema = eventSchema.omit({ id: true }).extend({
+  images: z.array(fileSchema.omit({ id: true })),
 });
+
+export const updateEventSchema = eventSchema
+  .omit({
+    schoolId: true,
+  })
+  .extend({ images: z.array(fileSchema.omit({ id: true })) });
