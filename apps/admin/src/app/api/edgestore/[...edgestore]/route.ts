74cde3a037a896abd5788491b420fe04f87a3824
Farhan Aziz <farhan@aththariq.com>
2024-01-22 10:58:20 +0700

    Add Edge Store client and update image bucket size

--- apps/admin/src/app/api/edgestore/[...edgestore]/route.ts
+++ apps/admin/src/app/api/edgestore/[...edgestore]/route.ts
@@ -1,18 +1,33 @@
 import { initEdgeStore } from "@edgestore/server";
 import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app";
+import { initEdgeStoreClient } from "@edgestore/server/core";
+import { z } from "zod";
 
 const es = initEdgeStore.create();
 /**
  * This is the main router for the Edge Store buckets.
  */
 const edgeStoreRouter = es.router({
-  publicFiles: es.imageBucket({
-    maxSize: 1024 * 1024 * 5, // 5MB
-  }),
+  publicImages: es
+    .imageBucket({
+      maxSize: 1024 * 1024 * 1, // 1MB
+    })
+    .input(
+      z.object({
+        category: z.string().min(1),
+      }),
+    )
+    .path(({ input }) => [{ category: input.category }]),
 });
+
 const handler = createEdgeStoreNextHandler({
   router: edgeStoreRouter,
 });
+
+export const backendClientES = initEdgeStoreClient({
+  router: edgeStoreRouter,
+});
+
 export { handler as GET, handler as POST };
 /**
  * This type is used to create the type-safe client for the frontend.
