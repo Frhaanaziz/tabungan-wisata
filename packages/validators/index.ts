36ebb7f0166afca1f78fa9f27a2f851e92c66872
Farhan Aziz <farhan@aththariq.com>
2024-01-23 13:48:58 +0700

    Update validators module

--- packages/validators/index.ts
+++ packages/validators/index.ts
@@ -1,8 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const paginatedDataUtilsSchema = z.object({
   currentPage: z.number().min(1),
   totalRow: z.number(),
   rowsPerPage: z.number(),
   totalPages: z.number(),
+});
+
+export const getPaginatedDataSchema = z.object({
+  page: z.coerce.number(),
+  take: z.coerce.number().min(1).optional(),
+  search: z.string().optional().default(''),
 });
