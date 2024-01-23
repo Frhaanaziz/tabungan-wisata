ad0aaefcfb072a46d4a19e4392724d70210b17db
Farhan Aziz <farhan@aththariq.com>
2024-01-23 13:47:03 +0700

    Refactor paymentRouter to use getPaginatedDataSchema

--- apps/admin/src/server/api/routers/payments.ts
+++ apps/admin/src/server/api/routers/payments.ts
@@ -1,18 +1,11 @@
-import { z } from "zod";
-
 import { createTRPCRouter, adminProcedure } from "@/server/api/trpc";
 import { getBackendApi } from "@/lib/axios";
 import { TRPCError } from "@trpc/server";
+import { getPaginatedDataSchema } from "@repo/validators";
 
 export const paymentRouter = createTRPCRouter({
   getAllPaginated: adminProcedure
-    .input(
-      z.object({
-        page: z.coerce.number(),
-        take: z.coerce.number().min(1).optional(),
-        search: z.string().optional().default(""),
-      }),
-    )
+    .input(getPaginatedDataSchema)
     .query(async ({ input, ctx }) => {
       const accessToken = ctx.session.accessToken;
       const { page, take, search } = input;
@@ -32,41 +25,4 @@ export const paymentRouter = createTRPCRouter({
         });
       }
     }),
-
-  //   create: adminProcedure
-  //     .input(addEventSchema)
-  //     .mutation(async ({ input, ctx }) => {
-  //       const accessToken = ctx.session.accessToken;
-
-  //       try {
-  //         const result = await getBackendApi(accessToken).post("/events", input);
-
-  //         return result.data;
-  //       } catch (error) {
-  //         throw new TRPCError({
-  //           code: "INTERNAL_SERVER_ERROR",
-  //           message: "Failed to create event",
-  //         });
-  //       }
-  //     }),
-
-  //   update: adminProcedure
-  //     .input(updateEventSchema)
-  //     .mutation(async ({ input, ctx }) => {
-  //       const accessToken = ctx.session.accessToken;
-
-  //       try {
-  //         const result = await getBackendApi(accessToken).put(
-  //           `/events/${input.id}`,
-  //           input,
-  //         );
-
-  //         return result.data;
-  //       } catch (error) {
-  //         throw new TRPCError({
-  //           code: "INTERNAL_SERVER_ERROR",
-  //           message: "Failed to update event",
-  //         });
-  //       }
-  //     }),
 });
