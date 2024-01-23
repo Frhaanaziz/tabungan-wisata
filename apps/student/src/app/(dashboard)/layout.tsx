ad6685811d8b60ca2f012fa9bfdd23b7e68659cd
Farhan Aziz <farhan@aththariq.com>
2024-01-23 13:48:05 +0700

    Add Footer component to DashboardLayout

--- apps/student/src/app/(dashboard)/layout.tsx
+++ apps/student/src/app/(dashboard)/layout.tsx
@@ -1,14 +1,15 @@
 import type { ReactNode } from "react";
 import { checkSessionAction } from "../_actions";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const DashboardLayout = async ({ children }: { children: ReactNode }) => {
   const session = await checkSessionAction();
-  console.log(session);
   return (
     <>
       <Header session={session} />
       {children}
+      <Footer />
     </>
   );
 };
