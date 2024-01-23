c97d69c4e02de9b2acff6527adf18e6d3e34301e
Farhan Aziz <farhan@aththariq.com>
2024-01-23 13:48:39 +0700

    Add payment router to appRouter

--- apps/student/src/server/api/root.ts
+++ apps/student/src/server/api/root.ts
@@ -2,11 +2,13 @@ import { createTRPCRouter } from "@/server/api/trpc";
 import { userRouter } from "./routers/users";
 import { authRouter } from "./routers/auth";
 import { eventRouter } from "./routers/events";
+import { paymentRouter } from "./routers/payments";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
   auth: authRouter,
   event: eventRouter,
+  payment: paymentRouter,
 });
 
 // export type definition of API
