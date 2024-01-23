12b9d7bb2c4cc4cdf6f705656dac3f7fc6d306e3
Farhan Aziz <farhan@aththariq.com>
2024-01-23 13:48:13 +0700

    Refactor event page layout and content

--- apps/student/src/app/(dashboard)/events/[id]/page.tsx
+++ apps/student/src/app/(dashboard)/events/[id]/page.tsx
@@ -5,26 +5,22 @@ import RichText from "@ui/components/RichText";
 const EventPage = async ({ params: { id } }: { params: { id: string } }) => {
   const event = await api.event.getOne.query(id);
   return (
-    <main className="container max-w-[1200px]">
-      <section className="my-10 grid items-center gap-10 sm:grid-cols-2">
+    <main className="container my-10 max-w-[1200px]">
+      <section className="mb-16 grid items-center gap-16 sm:grid-cols-2">
         <EventCarousel event={event} />
 
         <div>
           <p className="text-3xl font-bold">Included:</p>
           <div className="my-4 h-0.5 w-24 bg-primary" />
-          <ul className="clear-start flex list-inside list-image-[url(/icons/check-circle.svg)] flex-col gap-2">
-            <li>2-malam di hotel pilihan termasuk makan pagi sekamar berdua</li>
-            <li>Private Deluxe AC Car, Indonesia speaking Guide</li>
-            <li>Tiket masuk, parkir, 02 x Lunch di Local Restaurant</li>
-            <li>Transfer Airport – Hotel – Airport</li>
-            <li>Disediakan Hand Sanitizer dalam mobil</li>
-            <li>Airline Tickets</li>
-            <li>Airport tax & Additional baggage</li>
-            <li>Rapid Test / PCR Test</li>
-          </ul>
+          <RichText
+            content={event.include}
+            className="prose-li:list-image-[url(/icons/check-circle.svg)]"
+          />
         </div>
       </section>
 
+      <h1 className="text-3xl font-bold">{event.name}</h1>
+      <div className="my-4 h-0.5 w-24 bg-primary" />
       <RichText
         className={`prose-li:list-image-[url(/icons/check-circle.svg)]`}
         content={event.description}
