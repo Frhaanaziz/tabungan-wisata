18ca719f4c42eef6c49bc7ee4bb75012c5e204dc
Farhan Aziz <farhan@aththariq.com>
2024-01-23 13:48:24 +0700

    Add SearchDataTable component

---
+++ apps/student/src/components/data-table/SearchDataTable.tsx
@@ -0,0 +1,283 @@
+"use client";
+
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import type {
+  ColumnDef,
+  ColumnFiltersState,
+  PaginationState,
+  SortingState,
+  VisibilityState,
+} from "@tanstack/react-table";
+
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+  Settings2,
+} from "lucide-react";
+
+import { Button } from "@ui/components/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@ui/components/dropdown-menu";
+import { Input } from "@ui/components/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@ui/components/table";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import type { PaginatedDataUtils } from "@repo/types";
+
+export function SearchDataTable({
+  data,
+  columns,
+  utils,
+  SearchInput,
+  emptyState = "No results.",
+}: {
+  data: any[];
+  columns: ColumnDef<any>[];
+  utils: PaginatedDataUtils;
+  SearchInput: JSX.Element;
+  emptyState?: string;
+}) {
+  const { currentPage, rowsPerPage, totalPages } = utils;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+
+  const createQueryString: any = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
+    pageIndex: currentPage,
+    pageSize: rowsPerPage,
+  });
+
+  useEffect(() => {
+    router.push(
+      pathname + "?" + createQueryString("page", pageIndex.toString()),
+      { scroll: false },
+    );
+  }, [pageIndex, createQueryString, pathname, router]);
+
+  const pagination = useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize],
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    manualPagination: true,
+    pageCount: totalPages + 1,
+    onPaginationChange: setPagination,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+      pagination,
+    },
+  });
+
+  const canPreviousPage = pagination.pageIndex > 1;
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center justify-between pb-4">
+        <div className="flex">{SearchInput}</div>
+
+        <div className="flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                size="sm"
+                className="hidden gap-1.5 text-xs lg:flex"
+              >
+                <Settings2 className="h-4 w-4" /> View
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  {emptyState}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-between gap-5 overflow-x-auto py-4 ">
+        <div className="flex items-center gap-2">
+          <p className=" flex-shrink-0 font-semibold">
+            Page{" "}
+            <span>
+              {table.getState().pagination.pageIndex} of{" "}
+              {table.getPageCount() - 1}
+              {/* {table.getPageCount()} */}
+            </span>
+          </p>
+        </div>
+
+        <div className="hidden items-center gap-1 self-end sm:flex">
+          <p className="font-semibold">Go to page:</p>
+          <Input
+            type="number"
+            min={1}
+            defaultValue={table.getState().pagination.pageIndex}
+            onChange={(e) => {
+              //   const page = e.target.value ? Number(e.target.value) : 0;
+              const page = e.target.value ? Number(e.target.value) : 1;
+              table.setPageIndex(page);
+            }}
+            className="w-16"
+          />
+        </div>
+
+        <div className="flex-shrink-0 space-x-2">
+          <Button
+            size="icon"
+            // onClick={() => table.setPageIndex(0)}
+            onClick={() => table.setPageIndex(1)}
+            disabled={!canPreviousPage}
+            // disabled={!table.getCanPreviousPage()}
+            className="hidden lg:inline-flex"
+          >
+            <ChevronsLeft className="h-4 w-4" />
+          </Button>
+
+          <Button
+            size="icon"
+            onClick={() => table.previousPage()}
+            disabled={!canPreviousPage}
+            // disabled={!table.getCanPreviousPage()}
+          >
+            <ChevronLeft className="h-4 w-4" />
+          </Button>
+          <Button
+            size="icon"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <ChevronRight className="h-4 w-4" />
+          </Button>
+
+          <Button
+            size="icon"
+            onClick={() => table.setPageIndex(table.getPageCount())}
+            disabled={!table.getCanNextPage()}
+            className="hidden lg:inline-flex"
+          >
+            <ChevronsRight className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
