2aae426a0fc953b2d16e2eac33ca304eb973c6bd
Farhan Aziz <farhan@aththariq.com>
2024-01-23 13:46:36 +0700

    Refactor admin middleware to use withAuth callback

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -1,16 +1,10 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth(
-  // `withAuth` augments your `Request` with the user's token.
-  // function middleware(req) {
-  //   console.log(req.nextauth.token);
-  // },
-  {
-    callbacks: {
-      authorized: ({ token }: any) => token?.data?.role === "admin",
-    },
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }: any) => token?.data?.role === "admin",
   },
-);
+});
 
 export const config = {
   matcher: [
