e15dd282cfb6f6096274fc131df651b11a80fadf
Farhan Aziz <farhan@aththariq.com>
2024-01-24 17:21:06 +0700

    Add new endpoints and update existing ones in PaymentsController

--- apps/backend/src/payments/payments.controller.ts
+++ apps/backend/src/payments/payments.controller.ts
@@ -1,23 +1,34 @@
-import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { Admin } from 'src/auth/admin.decorator';
+import { Public } from 'src/auth/public.decorator';
+import { MidtransPaymentNotificationDto } from './dto/payment-notification.dto';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post()
-  create(@Body() createPaymentDto: CreatePaymentDto) {
-    const { userId, ...restPaymentData } = createPaymentDto;
+  async create(
+    @Body() createPaymentDto: CreatePaymentDto,
+    @Query('baseUrl') baseUrl: string,
+  ) {
+    if (!baseUrl)
+      throw new NotFoundException('baseUrl query string is required');
 
-    return this.paymentsService.createPayment({
-      ...restPaymentData,
-      user: {
-        connect: {
-          id: userId,
-        },
-      },
+    return this.paymentsService.createPaymentTransaction({
+      ...createPaymentDto,
+      baseUrl,
     });
   }
 
@@ -46,8 +57,32 @@ export class PaymentsController {
     return this.paymentsService.getPayments({});
   }
 
+  @Public()
+  @Post('notification')
+  async midtransPaymentNotification(
+    @Body() body: MidtransPaymentNotificationDto,
+  ) {
+    const { order_id, gross_amount, status_code } = body;
+    console.log(gross_amount, status_code);
+
+    const payment = await this.paymentsService.getPayment({ id: order_id });
+    if (payment) {
+      await this.paymentsService.updateStatusBasedOnMidtransResponse(
+        body,
+        payment,
+      );
+    }
+
+    return 'OK';
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.paymentsService.getPayment({ id });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.paymentsService.deletePayment({ id });
   }
 }
