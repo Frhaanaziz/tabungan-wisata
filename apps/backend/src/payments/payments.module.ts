01e79ea2062f6ab0498c3ec9cddac8b593dcdec1
Farhan Aziz <farhan@aththariq.com>
2024-01-24 17:21:12 +0700

    Add UsersModule and MidtransModule imports to PaymentsModule

--- apps/backend/src/payments/payments.module.ts
+++ apps/backend/src/payments/payments.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { UtilsModule } from 'src/utils/utils.module';
+import { UsersModule } from 'src/users/users.module';
+import { MidtransModule } from 'src/midtrans/midtrans.module';
 
 @Module({
-  imports: [UtilsModule],
+  imports: [UtilsModule, forwardRef(() => UsersModule), MidtransModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
