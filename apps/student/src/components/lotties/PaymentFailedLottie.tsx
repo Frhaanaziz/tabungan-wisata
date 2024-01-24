49b430c259221dd287b4244dc1ad9d8342fa637e
Farhan Aziz <farhan@aththariq.com>
2024-01-24 17:22:10 +0700

    Add PaymentFailedLottie component

---
+++ apps/student/src/components/lotties/PaymentFailedLottie.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Lottie from "react-lottie-player";
+import { useState, useEffect } from "react";
+import { Skeleton } from "@ui/components/skeleton";
+import { useRouter } from "next/navigation";
+
+const PaymentFailedLottie = () => {
+  const [animationData, setAnimationData] = useState<object>();
+  const router = useRouter();
+
+  // redirect to home page after 5 seconds
+  useEffect(() => {
+    import("@repo/assets/lotties/payment-failed.json").then(setAnimationData);
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  }, [router]);
+
+  if (!animationData)
+    return <Skeleton className="h-[300px] w-[300px] rounded-3xl" />;
+
+  return (
+    <Lottie
+      //  loop
+      animationData={animationData}
+      play
+      style={{ width: 300 }}
+    />
+  );
+};
+
+export default PaymentFailedLottie;
