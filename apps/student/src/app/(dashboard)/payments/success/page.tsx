676d90be4807cb0238639d4903b65ecb378ee470
Farhan Aziz <farhan@aththariq.com>
2024-01-24 17:22:00 +0700

    Add Payment Success Page

---
+++ apps/student/src/app/(dashboard)/payments/success/page.tsx
@@ -0,0 +1,26 @@
+import PaymentSuccessLottie from "@/components/lotties/PaymentSuccessLottie";
+import { buttonVariants } from "@ui/components/ui/button";
+import Link from "next/link";
+
+const PaymentSuccessPage = () => {
+  return (
+    <main className="">
+      <section className="flex min-h-[calc(100vh-8.6rem)] w-full flex-col items-center justify-center">
+        <PaymentSuccessLottie />
+        <p className="text-3xl font-medium text-green-500">Success</p>
+        <p className="my-3 font-medium text-primary">
+          Your payment has been processed successfully
+        </p>
+        <p className="text-balance text-center text-muted-foreground">
+          Page will be automatically redirected to the main page or click button
+          below
+        </p>
+        <Link href={"/"} className={buttonVariants({ className: "mt-5" })}>
+          Back to Home
+        </Link>
+      </section>
+    </main>
+  );
+};
+
+export default PaymentSuccessPage;
