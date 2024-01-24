278253b70cef030819a186fd0a1f225a41c27ac6
Farhan Aziz <farhan@aththariq.com>
2024-01-24 17:23:47 +0700

    Update payment schema and validators

--- packages/validators/payment.ts
+++ packages/validators/payment.ts
@@ -1,16 +1,30 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export enum PaymentStatus {
-  pending = "pending",
-  completed = "completed",
-  failed = "failed",
+  pending = 'pending',
+  completed = 'completed',
+  failed = 'failed',
 }
 
 export const paymentSchema = z.object({
   id: z.string().cuid(),
-  amount: z.number(),
-  date: z.date(),
+  amount: z.coerce
+    .number()
+    .int()
+    .min(1, { message: 'Amount must be greater than 0' })
+    .max(2147483647, { message: 'Amount must be less than 2147483647' }),
+  date: z.coerce.date(),
   userId: z.string().cuid(),
   status: z.nativeEnum(PaymentStatus),
   // user: userSchema.optional(),
+});
+
+export const addPaymentSchema = paymentSchema.pick({
+  amount: true,
+  userId: true,
+});
+
+export const updatePaymentSchema = paymentSchema.pick({
+  status: true,
+  id: true,
 });
