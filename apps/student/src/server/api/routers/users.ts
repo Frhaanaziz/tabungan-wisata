96793c5b95ef5287e9e04874e2dc0c173fa26741
Farhan Aziz <farhan@aththariq.com>
2024-01-25 16:44:30 +0700

    Remove commented out code in userRouter

--- apps/student/src/server/api/routers/users.ts
+++ apps/student/src/server/api/routers/users.ts
@@ -17,68 +17,11 @@ export const userRouter = createTRPCRouter({
         );
         return result.data;
       } catch (error) {
+        console.error("userRouter updateSchool", error);
         throw new TRPCError({
           code: "BAD_REQUEST",
           message: getNestErrorMessage(error),
         });
       }
     }),
-
-  // getAllPaginated: adminProcedure
-  //   .input(
-  //     z.object({
-  //       page: z.coerce.number(),
-  //       take: z.coerce.number().min(1).optional(),
-  //       search: z.string().optional().default(""),
-  //     }),
-  //   )
-  //   .query(async ({ input, ctx }) => {
-  //     const accessToken = ctx.session.accessToken;
-  //     const { page, take, search } = input;
-  //     try {
-  //       const result = await getBackendApi(accessToken, {
-  //         page,
-  //         take,
-  //         search,
-  //       }).get("/users");
-  //       return result.data;
-  //     } catch (error) {
-  //       throw new TRPCError({
-  //         code: "INTERNAL_SERVER_ERROR",
-  //         message: "Failed to get users",
-  //       });
-  //     }
-  //   }),
-  //   create: adminProcedure
-  //     .input(addSchoolSchema)
-  //     .mutation(async ({ input, ctx }) => {
-  //       const accessToken = ctx.session.accessToken;
-  //       try {
-  //         const result = await getBackendApi(accessToken).post("/users", input);
-  //         return result.data;
-  //       } catch (error) {
-  //         throw new TRPCError({
-  //           code: "INTERNAL_SERVER_ERROR",
-  //           message: "Failed to create school",
-  //         });
-  //       }
-  //     }),
-  //   update: adminProcedure
-  //     .input(updateSchoolSchema)
-  //     .mutation(async ({ input, ctx }) => {
-  //       const { id, ...rest } = input;
-  //       const accessToken = ctx.session.accessToken;
-  //       try {
-  //         const result = await getBackendApi(accessToken).put(
-  //           `/schools/${id}`,
-  //           rest,
-  //         );
-  //         return result.data;
-  //       } catch (error) {
-  //         throw new TRPCError({
-  //           code: "INTERNAL_SERVER_ERROR",
-  //           message: "Failed to update school",
-  //         });
-  //       }
-  //     }),
 });
