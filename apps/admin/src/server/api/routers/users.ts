9c0a9021ab29bf54ea776c324addf20fc287d39f
Farhan Aziz <farhan@aththariq.com>
2024-01-25 11:02:27 +0700

    Remove commented out code in userRouter

--- apps/admin/src/server/api/routers/users.ts
+++ apps/admin/src/server/api/routers/users.ts
@@ -19,48 +19,11 @@ export const userRouter = createTRPCRouter({
 
         return result.data;
       } catch (error) {
+        console.error("userRouter getAllPaginated", error);
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Failed to get users",
         });
       }
     }),
-
-  //   create: adminProcedure
-  //     .input(addSchoolSchema)
-  //     .mutation(async ({ input, ctx }) => {
-  //       const accessToken = ctx.session.accessToken;
-
-  //       try {
-  //         const result = await getBackendApi(accessToken).post("/users", input);
-
-  //         return result.data;
-  //       } catch (error) {
-  //         throw new TRPCError({
-  //           code: "INTERNAL_SERVER_ERROR",
-  //           message: "Failed to create school",
-  //         });
-  //       }
-  //     }),
-
-  //   update: adminProcedure
-  //     .input(updateSchoolSchema)
-  //     .mutation(async ({ input, ctx }) => {
-  //       const { id, ...rest } = input;
-  //       const accessToken = ctx.session.accessToken;
-
-  //       try {
-  //         const result = await getBackendApi(accessToken).put(
-  //           `/schools/${id}`,
-  //           rest,
-  //         );
-
-  //         return result.data;
-  //       } catch (error) {
-  //         throw new TRPCError({
-  //           code: "INTERNAL_SERVER_ERROR",
-  //           message: "Failed to update school",
-  //         });
-  //       }
-  //     }),
 });
