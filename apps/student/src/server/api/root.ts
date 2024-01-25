7cd6dcebbc5127dd5e7cb11658f834cdf383af30
Farhan Aziz <farhan@aththariq.com>
2024-01-25 15:20:43 +0700

    Add schoolRouter to appRouter

--- apps/student/src/server/api/root.ts
+++ apps/student/src/server/api/root.ts
@@ -3,12 +3,14 @@ import { userRouter } from "./routers/users";
 import { authRouter } from "./routers/auth";
 import { eventRouter } from "./routers/events";
 import { paymentRouter } from "./routers/payments";
+import { schoolRouter } from "./routers/school";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
   auth: authRouter,
   event: eventRouter,
   payment: paymentRouter,
+  school: schoolRouter,
 });
 
 // export type definition of API
