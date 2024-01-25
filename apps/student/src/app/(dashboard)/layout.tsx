813e1b65c6f44f2eefdba31e3e7226aba430f512
Farhan Aziz <farhan@aththariq.com>
2024-01-25 15:20:31 +0700

    Add school events to dashboard header

--- apps/student/src/app/(dashboard)/layout.tsx
+++ apps/student/src/app/(dashboard)/layout.tsx
@@ -2,12 +2,16 @@ import type { ReactNode } from "react";
 import { checkSessionAction } from "../_actions";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { api } from "@/trpc/server";
 
 const DashboardLayout = async ({ children }: { children: ReactNode }) => {
   const session = await checkSessionAction();
+  const schoolId = session.data.schoolId as string;
+
+  const events = await api.school.getEvents.query({ id: schoolId });
   return (
     <>
-      <Header session={session} />
+      <Header session={session} events={events} />
       {children}
       <Footer />
     </>
