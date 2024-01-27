50254433ba16009ed2cc3f0f282b9cf4e15d560c
Farhan Aziz <farhan@aththariq.com>
2024-01-27 13:53:35 +0700

    Refactor SchoolsTableSection component

--- apps/admin/src/components/section/SchoolsTableSection.tsx
+++ apps/admin/src/components/section/SchoolsTableSection.tsx
@@ -1,62 +1,30 @@
 "use client";
 import { Input } from "@ui/components/shadcn/input";
 import { SearchDataTable } from "../data-table/SearchDataTable";
-import { Suspense, useId, useState } from "react";
-import { toast } from "sonner";
 import type { SchoolsPaginated } from "@repo/types";
 import { schoolColumns } from "../data-table/columns/SchoolColumn";
-import { api } from "@/trpc/react";
-import { useDebounce } from "@/lib/hooks/useDebounce";
+import { useTableSection } from "@/lib/hooks/useTableSection";
 
-const SchoolsTableSection = ({
-  page,
-  initialData,
-}: {
-  page: number;
-  initialData: SchoolsPaginated;
-}) => {
-  const toastId = useId();
-  const [search, setSearch] = useState("");
-  const [data, setData] = useState(initialData);
-  const debouncedQuery = useDebounce(search);
-
-  api.school.getAllPaginated.useQuery(
-    {
-      page,
-      search: debouncedQuery,
-    },
-    {
-      queryKey: ["school.getAllPaginated", { page, search: debouncedQuery }],
-      onSuccess(data) {
-        setData(data);
-      },
-      onError(error) {
-        toast.error(error.message, { id: toastId });
-      },
-      initialData,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-    },
-  );
+const SchoolsTableSection = ({ data }: { data: SchoolsPaginated }) => {
+  const { search, setSearch } = useTableSection("");
 
   const { content, ...utils } = data;
 
   return (
-    <Suspense fallback={null}>
-      <SearchDataTable
-        data={content}
-        columns={schoolColumns}
-        utils={utils}
-        SearchInput={
-          <Input
-            placeholder="Filter names..."
-            value={search}
-            onChange={(event) => setSearch(event.target.value)}
-            className="max-w-sm"
-          />
-        }
-      />
-    </Suspense>
+    <SearchDataTable
+      data={content}
+      columns={schoolColumns}
+      utils={utils}
+      SearchInput={
+        <Input
+          placeholder="Filter names..."
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          className="max-w-sm"
+        />
+      }
+    />
   );
 };
+
 export default SchoolsTableSection;
