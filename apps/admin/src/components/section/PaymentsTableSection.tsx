76eb14be72c67cd5c694eaa4657c897085c2da2a
Farhan Aziz <farhan@aththariq.com>
2024-01-27 13:53:31 +0700

    Refactor PaymentsTableSection component

--- apps/admin/src/components/section/PaymentsTableSection.tsx
+++ apps/admin/src/components/section/PaymentsTableSection.tsx
@@ -1,43 +1,13 @@
 "use client";
 import { Input } from "@ui/components/shadcn/input";
 import { SearchDataTable } from "../data-table/SearchDataTable";
-import { Suspense, useId, useState } from "react";
-import { toast } from "sonner";
+import { Suspense } from "react";
 import type { PaymentsPaginated } from "@repo/types";
-import { api } from "@/trpc/react";
-import { useDebounce } from "@/lib/hooks/useDebounce";
 import { paymentColumn } from "../data-table/columns/PaymentColumn";
+import { useTableSection } from "@/lib/hooks/useTableSection";
 
-const PaymentsTableSection = ({
-  page,
-  initialData,
-}: {
-  page: number;
-  initialData: PaymentsPaginated;
-}) => {
-  const toastId = useId();
-  const [search, setSearch] = useState("");
-  const [data, setData] = useState(initialData);
-  const debouncedQuery = useDebounce(search);
-
-  api.payment.getAllPaginated.useQuery(
-    {
-      page,
-      search: debouncedQuery,
-    },
-    {
-      queryKey: ["payment.getAllPaginated", { page, search: debouncedQuery }],
-      onError(error) {
-        toast.error(error.message, { id: toastId });
-      },
-      onSuccess(data) {
-        setData(data);
-      },
-      initialData,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-    },
-  );
+const PaymentsTableSection = ({ data }: { data: PaymentsPaginated }) => {
+  const { search, setSearch } = useTableSection("");
 
   const { content, ...utils } = data;
 
