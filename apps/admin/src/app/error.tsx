0417dcf5940f5e8f8d4454714126c62504e466cf
Farhan Aziz <farhan@aththariq.com>
2024-01-28 09:47:51 +0700

    Add redirect for INVALID_ACCESS_TOKEN error

--- apps/admin/src/app/error.tsx
+++ apps/admin/src/app/error.tsx
@@ -2,6 +2,7 @@
 
 import { Button, buttonVariants } from "@ui/components/shadcn/button";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Error({
   error,
@@ -10,6 +11,8 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  if (error.message === "INVALID_ACCESS_TOKEN") redirect("auth/signin");
+
   return (
     <main className="grid min-h-[calc(100vh-4rem)] place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="rounded-xl border bg-accent p-10 text-center">
