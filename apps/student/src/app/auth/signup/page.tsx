099986196eaf491aa0c247aadc02697049fa8c86
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:48:56 +0700

    Update SignUpPage styling

--- apps/student/src/app/auth/signup/page.tsx
+++ apps/student/src/app/auth/signup/page.tsx
@@ -2,12 +2,8 @@ import SignUpForm from "@/components/forms/SignUpForm";
 
 const SignUpPage = () => {
   return (
-    <main className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 ">
-      <h1 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight ">
-        Create an account
-      </h1>
-
-      <section className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
+    <main className="flex min-h-screen flex-1 flex-col justify-center  bg-muted py-12 sm:px-6 lg:px-8 ">
+      <section className="mt-8 sm:mx-auto sm:w-full sm:max-w-[550px]">
         <div className="mx-6 border bg-card px-6 py-12 shadow sm:rounded-lg sm:px-12">
           <SignUpForm />
         </div>
