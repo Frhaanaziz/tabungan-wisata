2adefd38dafda8656d83754a7ab81ee1a38f6598
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:58:22 +0700

    Remove NextUIProvider component

--- apps/student/src/context/NextUIProvider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { NextUIProvider as NextUIProviderApi } from "@nextui-org/react";
-
-export function NextUIProvider({ children }: { children: React.ReactNode }) {
-  return <NextUIProviderApi>{children}</NextUIProviderApi>;
-}
