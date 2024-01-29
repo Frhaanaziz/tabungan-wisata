5f938a1f192fdbb033346d147e7a4740d1d64c6d
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:48:51 +0700

    Update sign-in page layout and add sign-up link

--- apps/student/src/app/auth/signin/page.tsx
+++ apps/student/src/app/auth/signin/page.tsx
@@ -1,4 +1,5 @@
 import SignInForm from "@/components/forms/SignInForm";
+import Link from "next/link";
 
 const SignInPage = ({
   searchParams,
@@ -9,16 +10,23 @@ const SignInPage = ({
   const error = searchParams.error;
 
   return (
-    <main className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 ">
-      <h1 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight ">
-        Sign in to your account
+    <main className="flex min-h-screen flex-1 flex-col justify-center bg-muted py-12 sm:px-6 lg:px-8 ">
+      <h1 className="mt-6 text-center text-4xl font-bold leading-9 tracking-tight ">
+        Welcome back
       </h1>
 
-      <section className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
+      <section className="mt-8 sm:mx-auto sm:w-full sm:max-w-[550px]">
         <div className="mx-6 border bg-card px-6 py-12 shadow sm:rounded-lg sm:px-12">
           <SignInForm callbackUrl={callbackUrl} error={error} />
         </div>
       </section>
+
+      <Link
+        href="/auth/signup"
+        className="mt-10 inline-block text-center text-sm font-semibold"
+      >
+        Don&apos;t have an account? Sign up
+      </Link>
     </main>
   );
 };
