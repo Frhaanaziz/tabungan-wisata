c3e77bc69a82cb4705b6e1c043f4d2b657c3120c
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:51:14 +0700

    Update thumbnailUrl validation in fileSchema

--- packages/validators/file.ts
+++ packages/validators/file.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 export const fileSchema = z.object({
   id: z.string().cuid(),
   url: z.string().url(),
-  thumbnailUrl: z.string().url().optional(),
+  thumbnailUrl: z.string().url().optional().nullable(),
   size: z.number().min(0),
   uploadedAt: z.coerce.date(),
   eventId: z.string().cuid().optional(),
