7cfdf22ecc3614f7118c7b78150ba19609a6db24
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:47:24 +0700

    Add dialog for adding schools

--- apps/admin/src/components/forms/AddSchoolForm.tsx
+++ apps/admin/src/components/forms/AddSchoolForm.tsx
@@ -12,16 +12,25 @@ import {
   FormLabel,
   FormMessage,
 } from "@ui/components/shadcn/form";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@ui/components/shadcn/dialog";
+import { buttonVariants } from "@ui/components/shadcn/button";
 import { Input } from "@ui/components/shadcn/input";
 import { addSchoolSchema } from "@repo/validators/school";
 import { Textarea } from "@ui/components/shadcn/textarea";
 import SubmitButton from "../SubmitButton";
 import { api } from "@/trpc/react";
 import { toast } from "sonner";
+import React from "react";
 
 type AddSchoolType = z.infer<typeof addSchoolSchema>;
 
 const AddSchoolForm = () => {
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+
   const defaultValues: AddSchoolType = {
     name: "",
     address: "",
@@ -38,8 +47,9 @@ const AddSchoolForm = () => {
 
   const { mutate, isLoading } = api.school.create.useMutation({
     onSuccess: async () => {
-      reset(defaultValues);
+      setDialogOpen(false);
       toast.success("School added successfully");
+      reset(defaultValues);
       await utils.school.invalidate();
     },
     onError: (err) => {
@@ -48,59 +58,64 @@ const AddSchoolForm = () => {
   });
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={handleSubmit((value) => mutate(value))}
-        className="space-y-8"
-      >
-        <div className="space-y-5">
-          <FormField
-            control={control}
-            name="name"
-            disabled={isLoading}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="Enter school name" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={control}
-            name="address"
-            disabled={isLoading}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Address</FormLabel>
-                <FormControl>
-                  <Textarea {...field} placeholder="Enter school address" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={control}
-            name="contact"
-            disabled={isLoading}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Contact</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="Enter school contact" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+      <DialogTrigger className={buttonVariants()}>+ Add</DialogTrigger>
+      <DialogContent>
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit((value) => mutate(value))}
+            className="space-y-8"
+          >
+            <div className="space-y-5">
+              <FormField
+                control={control}
+                name="name"
+                disabled={isLoading}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter school name" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={control}
+                name="address"
+                disabled={isLoading}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Address</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} placeholder="Enter school address" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={control}
+                name="contact"
+                disabled={isLoading}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Contact</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter school contact" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-        <SubmitButton isSubmitting={isLoading} />
-      </form>
-    </Form>
+            <SubmitButton isSubmitting={isLoading} />
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 };
 
