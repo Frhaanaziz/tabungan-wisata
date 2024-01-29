4cf897bf6dcb57e351deaba219fb82b0ad2bb7e8
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:48:46 +0700

    Update background color in AuthSchoolPage

--- apps/student/src/app/auth/school/page.tsx
+++ apps/student/src/app/auth/school/page.tsx
@@ -2,7 +2,7 @@ import AddSchoolForm from "@/components/forms/AddSchoolForm";
 
 const AuthSchoolPage = () => {
   return (
-    <main className="flex min-h-screen flex-col justify-center">
+    <main className="flex min-h-screen flex-col justify-center  bg-muted">
       <section className="mx-6 border bg-card px-6 py-12 shadow sm:mx-auto sm:w-full sm:max-w-[480px] sm:rounded-lg sm:px-12">
         <AddSchoolForm />
       </section>
