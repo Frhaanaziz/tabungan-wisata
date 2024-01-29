bfca9ce135bb609dd9e6978d39f222fff01153de
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:47:07 +0700

    Remove unused code and simplify Add School functionality

--- apps/admin/src/app/(dashboard)/schools/page.tsx
+++ apps/admin/src/app/(dashboard)/schools/page.tsx
@@ -1,11 +1,4 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from "@ui/components/shadcn/dialog";
-
 import AddSchoolForm from "@/components/forms/AddSchoolForm";
-import { buttonVariants } from "@ui/components/shadcn/button";
 import { api } from "@/trpc/server";
 import { Suspense } from "react";
 import DataTableSkeleton from "@/components/skeleton/DataTableSkeleton";
@@ -31,12 +24,7 @@ const SchoolsPage = async ({
       <header className="mb-4 flex items-center justify-between pb-5">
         <h1 className="text-2xl font-semibold leading-6 ">Schools</h1>
         <div className="mt-3 sm:ml-4 sm:mt-0">
-          <Dialog>
-            <DialogTrigger className={buttonVariants()}>+ Add</DialogTrigger>
-            <DialogContent>
-              <AddSchoolForm />
-            </DialogContent>
-          </Dialog>
+          <AddSchoolForm />
         </div>
       </header>
 
