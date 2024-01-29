7738fafa41aa06f8d3d08b5375d10e4932f52f80
Farhan Aziz <farhan@aththariq.com>
2024-01-29 14:51:06 +0700

    Add itinerary schema to event validator

--- packages/validators/event.ts
+++ packages/validators/event.ts
@@ -1,34 +1,41 @@
 import { z } from 'zod';
 import { fileSchema } from './file';
+import {
+  addItinerarySchema,
+  itinerarySchema,
+  updateItinerarySchema,
+} from './itinerary';
 export const eventSchema = z.object({
   id: z.string().cuid(),
   name: z
     .string()
     .min(1, { message: 'Name must have at least 1 character' })
     .max(190),
-  description: z
-    .string()
-    .min(1, { message: 'Description must have at least 1 character' }),
+  highlight: z.string().min(1, {
+    message: 'Highlight must have at least 1 character',
+  }),
   include: z.string().min(1, {
     message: 'Include must have at least 1 character',
+  }),
+  exclude: z.string().min(1, {
+    message: 'Exclude must have at least 1 character',
   }),
   startDate: z.coerce.date(),
   endDate: z.coerce.date(),
   cost: z.coerce.number().min(0),
   schoolId: z.string().cuid({ message: 'Please select a school' }),
-  images: z
-    // .array(fileSchema.omit({ id: true }))
-    .array(fileSchema),
-  // .min(1, { message: 'Please upload at least 1 image' }),
-  // registrations: z.array(eventRegistrationSchema).optional(),
-  // school: schoolSchema.optional(),
+  images: z.array(fileSchema),
+  itineraries: z.array(itinerarySchema),
 });
 
-export const addEventSchema = eventSchema.omit({ id: true }).extend({
-  images: z.array(fileSchema.omit({ id: true })).min(1, {
-    message: 'Please upload at least 1 image',
-  }),
-});
+export const addEventSchema = eventSchema
+  .omit({ id: true, itineraries: true })
+  .extend({
+    images: z.array(fileSchema.omit({ id: true })).min(1, {
+      message: 'Please upload at least 1 image',
+    }),
+    itineraries: z.array(addItinerarySchema),
+  });
 
 export const updateEventSchema = eventSchema
   .omit({
@@ -38,4 +45,5 @@ export const updateEventSchema = eventSchema
     images: z
       .array(fileSchema.omit({ id: true }))
       .min(1, { message: 'Please upload at least 1 image' }),
+    itineraries: z.array(updateItinerarySchema),
   });
