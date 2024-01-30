d0aedc93217b7927251389e6ae5ef2e78145fe88
Farhan Aziz <farhan@aththariq.com>
2024-01-30 15:48:10 +0700

    Add withdrawal router to appRouter

--- apps/admin/src/server/api/root.ts
+++ apps/admin/src/server/api/root.ts
@@ -3,12 +3,14 @@ import { schoolRouter } from "./routers/schools";
 import { userRouter } from "./routers/users";
 import { eventRouter } from "./routers/events";
 import { paymentRouter } from "./routers/payments";
+import { withdrawalRouter } from "./routers/withdrawals";
 
 export const appRouter = createTRPCRouter({
   school: schoolRouter,
   user: userRouter,
   event: eventRouter,
   payment: paymentRouter,
+  withdrawal: withdrawalRouter,
 });
 
 // export type definition of API
