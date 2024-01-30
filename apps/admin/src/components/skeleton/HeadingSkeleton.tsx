ea49b0fce45a129cd6c295fd3bbfa6d644d93ab2
Farhan Aziz <farhan@aththariq.com>
2024-01-30 11:25:30 +0700

    Add loading components for dashboard events, payments, schools, and users

---
+++ apps/admin/src/components/skeleton/HeadingSkeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@ui/components/shadcn/skeleton";
+
+const HeadingSkeleton = () => {
+  return <Skeleton className="mb-[36px] h-[24px] w-[100px]" />;
+};
+
+export default HeadingSkeleton;
