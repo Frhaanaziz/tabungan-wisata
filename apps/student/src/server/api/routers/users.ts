c178c68845e19ea51abd1b57e7f71acd9d1e9718
Farhan Aziz <farhan@aththariq.com>
2024-01-30 15:48:24 +0700

    Remove unused getBalance function

--- apps/student/src/server/api/routers/users.ts
+++ apps/student/src/server/api/routers/users.ts
@@ -24,21 +24,4 @@ export const userRouter = createTRPCRouter({
         });
       }
     }),
-
-  getBalance: privateProcedure.query(async ({ ctx }) => {
-    const user = ctx.session.data;
-    const accessToken = ctx.session.accessToken;
-    try {
-      const result = await getBackendApi(accessToken).get(
-        `/users/${user.id}/balance`,
-      );
-      return result.data;
-    } catch (error) {
-      console.error("userRouter getBalance", error);
-      throw new TRPCError({
-        code: "BAD_REQUEST",
-        message: getNestErrorMessage(error),
-      });
-    }
-  }),
 });
