6fe1bba1f6a13652e833499df831ea98593eb2da
Farhan Aziz <farhan@aththariq.com>
2024-01-31 16:13:48 +0700

    Add withdrawal creation and total amount retrieval endpoints

--- apps/admin/src/server/api/routers/withdrawals.ts
+++ apps/admin/src/server/api/routers/withdrawals.ts
@@ -2,6 +2,11 @@ import { createTRPCRouter, adminProcedure } from "@/server/api/trpc";
 import { getBackendApi } from "@/lib/axios";
 import { TRPCError } from "@trpc/server";
 import { getPaginatedDataSchema } from "@repo/validators";
+import {
+  addWithdrawalSchema,
+  withdrawalSchema,
+} from "@repo/validators/withdrawal";
+import { z } from "zod";
 
 export const withdrawalRouter = createTRPCRouter({
   getAllPaginated: adminProcedure
@@ -23,6 +28,49 @@ export const withdrawalRouter = createTRPCRouter({
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Failed to get withdrawals",
+        });
+      }
+    }),
+
+  create: adminProcedure
+    .input(addWithdrawalSchema)
+    .mutation(async ({ input, ctx }) => {
+      const accessToken = ctx.session.accessToken;
+
+      try {
+        const result = await getBackendApi(accessToken).post(
+          "/withdrawals",
+          input,
+        );
+
+        return result.data;
+      } catch (error) {
+        console.error("withdrawalRouter create", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to create withdrawal",
+        });
+      }
+    }),
+
+  getTotalAmount: adminProcedure
+    .input(withdrawalSchema.pick({ schoolId: true }))
+    .output(z.number())
+    .query(async ({ input, ctx }) => {
+      const accessToken = ctx.session.accessToken;
+
+      try {
+        const result = await getBackendApi(accessToken).get(
+          `/withdrawals/total-amount`,
+          { data: input },
+        );
+
+        return result.data;
+      } catch (error) {
+        console.error("withdrawalRouter getTotalAmount", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to get total amount",
         });
       }
     }),
