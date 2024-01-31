941eb34478518898897406c33de7c6531e4acb9a
Farhan Aziz <farhan@aththariq.com>
2024-01-31 16:14:14 +0700

    Add getBalance endpoint to userRouter

--- apps/student/src/server/api/routers/users.ts
+++ apps/student/src/server/api/routers/users.ts
@@ -3,6 +3,7 @@ import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
 import { getNestErrorMessage } from "@repo/utils";
 import { AddSchoolCodeSchema } from "@repo/validators/auth";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
   updateSchool: privateProcedure
@@ -20,8 +21,25 @@ export const userRouter = createTRPCRouter({
         console.error("userRouter updateSchool", error);
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: getNestErrorMessage(error),
+          message: "Something went wrong. Please try again later.",
         });
       }
     }),
+
+  getBalance: privateProcedure.output(z.number()).query(async ({ ctx }) => {
+    const user = ctx.session.data;
+    const accessToken = ctx.session.accessToken;
+    try {
+      const result = await getBackendApi(accessToken).get(
+        `/users/${user.id}/balance`,
+      );
+      return result.data;
+    } catch (error) {
+      console.error("userRouter getBalance", error);
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: getNestErrorMessage(error),
+      });
+    }
+  }),
 });
