dbb156173e19dcfc9239cee4976745f2a740be67
Farhan Aziz <farhan@aththariq.com>
2024-01-31 16:14:22 +0700

    Add school validation to withdrawal schema

--- packages/validators/withdrawal.ts
+++ packages/validators/withdrawal.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { userSchema } from './user';
+import { schoolSchema } from './school';
 
 export const withdrawalSchema = z.object({
   id: z.string().cuid(),
@@ -9,12 +10,14 @@ export const withdrawalSchema = z.object({
     .min(1, { message: 'Amount must be greater than 0' })
     .max(2147483647, { message: 'Amount must be less than 2147483647' }),
   userId: z.string().cuid(),
+  schoolId: z.string().cuid({ message: 'Please select a school' }),
   createdAt: z.coerce.date(),
   updatedAt: z.coerce.date(),
   user: userSchema,
+  school: schoolSchema,
 });
 
 export const addWithdrawalSchema = withdrawalSchema.pick({
-  amount: true,
   userId: true,
+  schoolId: true,
 });
