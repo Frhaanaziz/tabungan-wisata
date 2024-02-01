2bfee252b82c255e45c9e1be76141387dc93eee0
Farhan Aziz <farhan@aththariq.com>
2024-02-01 18:45:59 +0700

    Add new API endpoints for user statistics

--- apps/admin/src/server/api/routers/users.ts
+++ apps/admin/src/server/api/routers/users.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter, adminProcedure } from "@/server/api/trpc";
 import { getBackendApi } from "@/lib/axios";
 import { TRPCError } from "@trpc/server";
 import { getPaginatedDataSchema } from "@repo/validators";
+import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
   getAllPaginated: adminProcedure
@@ -23,6 +24,87 @@ export const userRouter = createTRPCRouter({
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Failed to get users",
+        });
+      }
+    }),
+
+  getCountNewUsers: adminProcedure
+    .input(z.object({ days: z.coerce.number() }))
+    .output(z.coerce.number())
+    .query(async ({ input, ctx }) => {
+      const accessToken = ctx.session.accessToken;
+
+      try {
+        const result = await getBackendApi(accessToken, input).get(
+          "/users/count-new-users",
+        );
+
+        return result.data;
+      } catch (error) {
+        console.error("userRouter getCountNewUsers", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to get count new users",
+        });
+      }
+    }),
+
+  getGrowthPercentage: adminProcedure
+    .output(z.coerce.number())
+    .query(async ({ ctx }) => {
+      const accessToken = ctx.session.accessToken;
+
+      try {
+        const result = await getBackendApi(accessToken).get(
+          "/users/growth-percentage",
+        );
+
+        return result.data;
+      } catch (error) {
+        console.error("userRouter getGrowthPercentage", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to get growth percentage",
+        });
+      }
+    }),
+
+  getGrowthCount: adminProcedure
+    .input(z.object({ days: z.coerce.number() }))
+    .output(z.coerce.number())
+    .query(async ({ input, ctx }) => {
+      const accessToken = ctx.session.accessToken;
+
+      try {
+        const result = await getBackendApi(accessToken, input).get(
+          "/users/growth-count",
+        );
+
+        return result.data;
+      } catch (error) {
+        console.error("userRouter getGrowthCount", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to get growth count",
+        });
+      }
+    }),
+
+  getTotalUsers: adminProcedure
+    .output(z.coerce.number())
+    .query(async ({ ctx }) => {
+      const accessToken = ctx.session.accessToken;
+
+      try {
+        const result =
+          await getBackendApi(accessToken).get("/users/total-users");
+
+        return result.data;
+      } catch (error) {
+        console.error("userRouter getTotalUsers", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to get total users",
         });
       }
     }),
