63a823b1a690f2adaf62100f448edb4c95c1a3fb
Farhan Aziz <farhan@aththariq.com>
2024-02-02 21:47:21 +0700

    Remove unused imports and commented code in school.ts

--- apps/student/src/server/api/routers/school.ts
+++ apps/student/src/server/api/routers/school.ts
@@ -1,30 +1,8 @@
-import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
-import { getBackendApi } from "@/lib/axios";
-import { TRPCError } from "@trpc/server";
-import { schoolSchema } from "@repo/validators/school";
-import { z } from "zod";
-import { eventSchema } from "@repo/validators/event";
+import { createTRPCRouter } from "@/server/api/trpc";
+// import { getBackendApi } from "@/lib/axios";
+// import { TRPCError } from "@trpc/server";
+// import { schoolSchema } from "@repo/validators/school";
+// import { z } from "zod";
+// import { eventSchema } from "@repo/validators/event";
 
-export const schoolRouter = createTRPCRouter({
-  getEvents: privateProcedure
-    .input(schoolSchema.pick({ id: true }))
-    .output(z.array(eventSchema.omit({ itineraries: true })))
-    .query(async ({ ctx, input }) => {
-      const { id: schoolId } = input;
-      const accessToken = ctx.session.accessToken;
-
-      try {
-        const result = await getBackendApi(accessToken).get(
-          `/schools/${schoolId}/events`,
-        );
-
-        return result.data;
-      } catch (error) {
-        console.error("schoolRouter getEvents", error);
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to get events for school",
-        });
-      }
-    }),
-});
+export const schoolRouter = createTRPCRouter({});
