d96b0345a5b6a334f754430a0919af058e6cff79
Farhan Aziz <farhan@aththariq.com>
2024-02-02 21:47:31 +0700

    Update import statements and coerce createdAt and updatedAt to dates

--- packages/validators/school.ts
+++ packages/validators/school.ts
@@ -1,6 +1,6 @@
-import { z } from "zod";
-import { userSchema } from "./user";
-import { eventSchema } from "./event";
+import { z } from 'zod';
+import { userSchema } from './user';
+import { eventSchema } from './event';
 
 // create a custom error message
 export const schoolSchema = z.object({
@@ -8,20 +8,20 @@ export const schoolSchema = z.object({
   code: z.string().min(1).max(190),
   name: z
     .string()
-    .min(1, { message: "Name must have at least 1 character" })
+    .min(1, { message: 'Name must have at least 1 character' })
     .max(190),
   address: z
     .string()
-    .min(1, { message: "Address must have at least 1 character" })
+    .min(1, { message: 'Address must have at least 1 character' })
     .max(190),
   contact: z
     .string()
-    .min(1, { message: "Contact must have at least 1 character" })
+    .min(1, { message: 'Contact must have at least 1 character' })
     .max(190),
   users: z.array(userSchema).optional(),
   events: z.array(eventSchema).optional(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
 });
 
 export const addSchoolSchema = schoolSchema.pick({
