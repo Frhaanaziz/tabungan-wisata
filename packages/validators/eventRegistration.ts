4de489e38ba38f990fb5bcdf1a63e59f3158d9cb
Farhan Aziz <farhan@aththariq.com>
2024-02-02 21:47:27 +0700

    Refactor eventRegistrationSchema in eventRegistration.ts

--- packages/validators/eventRegistration.ts
+++ packages/validators/eventRegistration.ts
@@ -1,9 +1,15 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const eventRegistrationSchema = z.object({
   id: z.string().cuid(),
-  eventId: z.string().cuid(),
-  userId: z.string().cuid(),
-  // event: eventSchema,
-  // user: userSchema.optional(),
+  eventId: z.string().cuid({ message: 'Please select an event' }),
+  schoolId: z.string().cuid(),
+  cost: z.coerce.number().min(1, { message: 'Cost must be at least 1' }),
+  createdAt: z.coerce.date(),
+});
+
+export const addEventRegistrationSchema = eventRegistrationSchema.pick({
+  eventId: true,
+  schoolId: true,
+  cost: true,
 });
