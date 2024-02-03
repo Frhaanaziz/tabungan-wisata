d52be35ca6ac211a2fd07c19bef1e579898b42da
Farhan Aziz <farhan@aththariq.com>
2024-02-03 13:45:38 +0700

    Refactor TopSellingSection component to fetch events data and update destinations

--- apps/front/components/sections/TopSellingSection.tsx
+++ apps/front/components/sections/TopSellingSection.tsx
@@ -1,33 +1,51 @@
-import React from "react";
-import DestinationCard from "../cards/DestinationCard";
+import React from 'react';
+import DestinationCard from '../cards/DestinationCard';
+import { getEventsAction } from '@/app/_actions/event';
+import { getDaysBetweenDates, toRupiahSuffix, truncate } from '@repo/utils';
+import { notFound } from 'next/navigation';
 
-function TopSellingSection() {
-  const destinations = [
-    {
-      id: 0,
-      imageUrl: "/images/rome.png",
-      title: "Rome, Italy",
-      amount: "$5.42k",
-      duration: "10 Days Trip",
-      highlighted: false,
-    },
-    {
-      id: 1,
-      imageUrl: "/images/london.jpg",
-      title: "London, UK",
-      amount: "$4.2k",
-      duration: "12 Days Trip",
-      highlighted: false,
-    },
-    {
-      id: 2,
-      imageUrl: "/images/europe.png",
-      title: "Full Europe",
-      amount: "$15k",
-      duration: "28 Days Trip",
-      highlighted: true,
-    },
-  ];
+async function TopSellingSection() {
+  const { data: events } = await getEventsAction();
+  if (!events) return notFound();
+
+  const destinations =
+    events
+      .map((event) => ({
+        id: event.id,
+        imageUrl: event.images.at(1)?.url ?? '/',
+        title: truncate(event.name, 18),
+        amount: toRupiahSuffix(event.cost),
+        duration: `${getDaysBetweenDates(event.startDate, event.endDate)} Days Trip`,
+        highlighted: true,
+      }))
+      .slice(0, 3) ?? [];
+
+  // const destinations = [
+  //   {
+  //     id: 0,
+  //     imageUrl: '/images/rome.png',
+  //     title: 'Rome, Italy',
+  //     amount: '$5.42k',
+  //     duration: '10 Days Trip',
+  //     highlighted: false,
+  //   },
+  //   {
+  //     id: 1,
+  //     imageUrl: '/images/london.jpg',
+  //     title: 'London, UK',
+  //     amount: '$4.2k',
+  //     duration: '12 Days Trip',
+  //     highlighted: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     imageUrl: '/images/europe.png',
+  //     title: 'Full Europe',
+  //     amount: '$15k',
+  //     duration: '28 Days Trip',
+  //     highlighted: true,
+  //   },
+  // ];
   return (
     <section>
       <p className="text-lightGray text-[1.125rem] font-[600] text-center">
