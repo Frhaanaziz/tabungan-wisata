42e029acbc0ce9005f1c17aea623fdb9a0ea825f
Farhan Aziz <farhan@aththariq.com>
2024-02-03 13:45:41 +0700

    Add axios configuration for backend and API calls

---
+++ apps/front/lib/axios.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { env } from '@/env.mjs';
+
+export function getBackendApi(token?: string, params?: object) {
+  return axios.create({
+    baseURL: env.BACKEND_URL,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+    params,
+  });
+}
+
+export function getApi(token: string) {
+  return axios.create({
+    baseURL: '/api',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+  });
+}
