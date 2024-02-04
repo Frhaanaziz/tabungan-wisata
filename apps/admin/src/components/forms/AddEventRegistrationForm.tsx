e0d9c74c0e772cbb66c04a041682b69f69d0bc65
Farhan Aziz <farhan@aththariq.com>
2024-02-04 13:48:26 +0700

    Add event registration form fields and functionality

--- apps/admin/src/components/forms/AddEventRegistrationForm.tsx
+++ apps/admin/src/components/forms/AddEventRegistrationForm.tsx
@@ -13,6 +13,15 @@ import {
   FormLabel,
   FormMessage,
 } from "@ui/components/shadcn/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@ui/components/shadcn/popover";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { cn } from "@repo/utils";
+import { Calendar } from "@ui/components/shadcn/calendar";
 import { Input } from "@ui/components/shadcn/input";
 import SubmitButton from "../SubmitButton";
 import { api } from "@/trpc/react";
@@ -20,6 +29,8 @@ import { toast } from "sonner";
 import React from "react";
 import { addEventRegistrationSchema } from "@repo/validators/eventRegistration";
 import EventFormField from "./EventFormField";
+import FormFieldWrapper from "./FormFieldWrapper";
+import { Button } from "@ui/components/shadcn/button";
 
 type AddEventRegistrationType = z.infer<typeof addEventRegistrationSchema>;
 
@@ -33,6 +44,8 @@ const AddEventRegistrationForm = ({ schoolId, setDialogOpen }: Props) => {
     cost: 0,
     eventId: "",
     schoolId,
+    startDate: new Date(),
+    endDate: new Date(),
   };
 
   const utils = api.useUtils();
@@ -64,23 +77,109 @@ const AddEventRegistrationForm = ({ schoolId, setDialogOpen }: Props) => {
         className="space-y-8"
       >
         <div className="space-y-5">
-          <FormField
-            control={control}
-            name="cost"
-            disabled={isLoading}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Event cost</FormLabel>
-                <FormControl>
-                  <Input {...field} type="number" min={0} />
-                </FormControl>
-                <FormDescription>
-                  Set the event cost per student on Rp
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <FormFieldWrapper>
+            <FormField
+              control={form.control}
+              name="startDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Event start Date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        // disabled={(date) =>
+                        //   date > new Date() || date < new Date("1900-01-01")
+                        // }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="endDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Event end date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        // disabled={(date) =>
+                        //   date > new Date() || date < new Date("1900-01-01")
+                        // }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="cost"
+              disabled={isLoading}
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Event cost</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="number" min={0} />
+                  </FormControl>
+                  <FormDescription>
+                    Set the event cost per student on Rp
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FormFieldWrapper>
 
           <EventFormField events={events} control={control} name="eventId" />
         </div>
