6ebca4a7b5c7649daabe5e526d128d16600d6adc
Farhan Aziz <farhan@aththariq.com>
2024-02-04 13:48:53 +0700

    Add startDate and endDate fields to eventRegistrationSchema

--- packages/validators/eventRegistration.ts
+++ packages/validators/eventRegistration.ts
@@ -5,11 +5,15 @@ export const eventRegistrationSchema = z.object({
   eventId: z.string().cuid({ message: 'Please select an event' }),
   schoolId: z.string().cuid(),
   cost: z.coerce.number().min(1, { message: 'Cost must be at least 1' }),
+  startDate: z.coerce.date(),
+  endDate: z.coerce.date(),
   createdAt: z.coerce.date(),
 });
 
 export const addEventRegistrationSchema = eventRegistrationSchema.pick({
   eventId: true,
   schoolId: true,
+  startDate: true,
+  endDate: true,
   cost: true,
 });
