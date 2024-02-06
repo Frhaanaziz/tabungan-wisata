9ee95bfb65d4324a9e79c6957d0af0cf5de3ecd8
Farhan Aziz <farhan@aththariq.com>
2024-02-06 21:50:03 +0700

    Add GoogleMap component

---
+++ packages/ui/components/GoogleMap.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import {
+  GoogleMap as GoogleMapApi,
+  useJsApiLoader,
+} from '@react-google-maps/api';
+import { Skeleton } from '@ui/components/shadcn/skeleton';
+import React from 'react';
+
+const containerStyle = {
+  width: '300px',
+  height: '300px',
+};
+
+const center = {
+  // lat: -3.745,
+  // lng: -38.523,
+  lat: -6.9174,
+  lng: 107.7133,
+};
+
+const GoogleMap = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+  });
+
+  // const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map: any) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    // setMap(map);
+  }, []);
+
+  // const onUnmount = React.useCallback(function callback(map: any) {
+  //   setMap(null);
+  // }, []);
+
+  return isLoaded ? (
+    <GoogleMapApi
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={7}
+      onLoad={onLoad}
+      // onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMapApi>
+  ) : (
+    <Skeleton className="h-[300px] w-[300px]" />
+  );
+};
+
+export default GoogleMap;
