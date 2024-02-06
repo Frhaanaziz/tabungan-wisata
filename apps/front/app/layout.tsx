6b0dbbbde271eeb4e3e868c1c22eeedff592b704
Farhan Aziz <farhan@aththariq.com>
2024-02-06 21:47:41 +0700

    Update font imports and add NextTopLoader and NavBar components

--- apps/front/app/layout.tsx
+++ apps/front/app/layout.tsx
@@ -1,23 +1,51 @@
-import type { Metadata } from 'next'
-import { Poppins, Volkhov } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Poppins, Volkhov, Caveat } from 'next/font/google';
+import './globals.css';
+import NextTopLoader from 'nextjs-toploader';
+import NavBar from '@/components/common/NavBar';
+import { cn } from '@repo/utils';
+import { webPrimaryColor } from '@repo/utils/constants';
 
-const poppins = Poppins({ subsets: ['latin'], weight: ["400", "500", "600", "700"] })
-const volkhov = Volkhov({ subsets: ['latin'], weight: ["400", "700"] })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+});
+const volkhov = Volkhov({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+});
+const caveat = Caveat({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-caveat',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Travel Agency Landing Page',
-  description: 'A Travel Agency Landing Page built with Next14, Shadcn, and Tailwind.',
-}
+  description:
+    'A Travel Agency Landing Page built with Next14, Shadcn, and Tailwind.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return (   
+  return (
     <html lang="en">
-      <body className={[poppins.className, volkhov.className].toString()} >{children}</body>
+      <body
+        className={cn(
+          [poppins.className, volkhov.className].toString(),
+          `${caveat.variable}`
+        )}
+      >
+        <NextTopLoader color={webPrimaryColor} />
+        <NavBar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
