d478699d4f9dda54053c58e3286104f2a0c0b7b4
Farhan Aziz <farhan@aththariq.com>
2024-02-06 21:47:48 +0700

    Refactor event actions to handle paginated data

--- apps/front/app/_actions/event.ts
+++ apps/front/app/_actions/event.ts
@@ -2,13 +2,37 @@ import { getBackendApi } from '@/lib/axios';
 import { getNestErrorMessage } from '@repo/utils';
 import { z } from 'zod';
 import { eventSchema } from '@repo/validators/event';
+import { paginatedDataUtilsSchema } from '@repo/validators';
 
 export async function getEventsAction() {
   try {
-    const { data: events } = await getBackendApi().get('/events');
+    const { data } = await getBackendApi().get('/events');
+    const events = z.array(eventSchema).parse(data);
 
-    const parsedEvents = z.array(eventSchema).parse(events);
-    return { data: parsedEvents, error: null };
+    return { data: events, error: null };
+  } catch (error) {
+    console.error('getEventsAction', error);
+    return { data: null, error: getNestErrorMessage(error) };
+  }
+}
+
+export async function getEventsPaginatedAction(
+  paginate: {
+    page?: number;
+    take?: number;
+    costLTE?: number;
+    costGTE?: number;
+    durationLTE?: number;
+    durationGTE?: number;
+  } = {}
+) {
+  try {
+    const { data } = await getBackendApi(undefined, paginate).get('/events');
+    const eventsPaginated = paginatedDataUtilsSchema
+      .extend({ content: z.array(eventSchema) })
+      .parse(data);
+
+    return { data: eventsPaginated, error: null };
   } catch (error) {
     console.error('getEventsAction', error);
     return { data: null, error: getNestErrorMessage(error) };
