d021a34e5f4d4c98971899c74bf8f5190007e77c
Farhan Aziz <farhan@aththariq.com>
2024-02-07 15:06:30 +0700

    Fix import paths and update descriptions in CategorySection component

--- apps/front/components/sections/CategorySection.tsx
+++ apps/front/components/sections/CategorySection.tsx
@@ -1,38 +1,38 @@
-import React from "react";
-import CatergoryCard from "../cards/CatergoryCard";
+import React from 'react';
+import CatergoryCard from '../cards/CatergoryCard';
 
 function CategorySection() {
   const features = [
     {
       id: 0,
-      iconUrl: "/images/satellite.png",
-      title: "Calculated Weather",
+      iconUrl: '/images/satellite.png',
+      title: 'Calculated Weather',
       description:
-        "Built Wicket longer admire do barton vanity itself do in it.",
+        'Built Wicket longer admire do barton vanity itself do in it.',
       highlighted: false,
     },
     {
       id: 1,
-      iconUrl: "/images/aircraft.png",
-      title: "Best Flights",
+      iconUrl: '/images/aircraft.png',
+      title: 'Best Flights',
       description:
-        "Engrossed listening. Park gate sell they west hard for the.",
-      highlighted: true,
+        'Engrossed listening. Park gate sell they west hard for the.',
+      highlighted: false,
     },
     {
       id: 2,
-      iconUrl: "/images/mic.png",
-      title: "Local Events",
+      iconUrl: '/images/mic.png',
+      title: 'Local Events',
       description:
-        "Barton vanity itself do in it. Preferd to men it engrossed listening. ",
+        'Barton vanity itself do in it. Preferd to men it engrossed listening. ',
       highlighted: false,
     },
     {
       id: 3,
-      iconUrl: "/images/cog.png",
-      title: "Customization",
+      iconUrl: '/images/cog.png',
+      title: 'Customization',
       description:
-        "We deliver outsourced aviation services for military customers",
+        'We deliver outsourced aviation services for military customers',
       highlighted: false,
     },
   ];
