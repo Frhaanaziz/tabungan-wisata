40c144ef3fab2744c7070168c7789f5dabdb1562
Farhan Aziz <farhan@aththariq.com>
2024-02-07 15:06:23 +0700

    Add NavBar component and restructure page layout

--- apps/front/app/page.tsx
+++ apps/front/app/page.tsx
@@ -1,3 +1,4 @@
+import NavBar from '@/components/common/NavBar';
 import BookNextTripSection from '@/components/sections/BookNextTripSection';
 import CategorySection from '@/components/sections/CategorySection';
 import FooterSection from '@/components/sections/FooterSection';
@@ -9,32 +10,35 @@ import TopSellingSection from '@/components/sections/TopSellingSection';
 
 export default function Home() {
   return (
-    <main className="relative poppins  2xl:px-[9rem] xl:px-4rem] lg:px-[3rem] md:px-[2rem]">
-      <div className="px-4 flex flex-col gap-[7.69rem]">
-        <HeroSection />
-        <div className="absolute top-0 right-0 -z-10">
-          <img src="/images/blob-shape.png" alt="blob background shape" />
-        </div>
-        <div className="absolute top-0 left-0 -z-10">
-          <img
-            src="/images/top-left-gradient.png"
-            alt="blob background shape"
-          />
-        </div>
-        <div className="relative">
-          <CategorySection />
-          <div className="absolute top-0 right-0">
-            <img src="/images/plus-group.png" alt="blob background shape" />
+    <>
+      <NavBar />
+      <main className="relative poppins  2xl:px-[9rem] xl:px-4rem] lg:px-[3rem] md:px-[2rem]">
+        <div className="px-4 flex flex-col gap-[7.69rem]">
+          <HeroSection />
+          <div className="absolute top-0 right-0 -z-10">
+            <img src="/images/blob-shape.png" alt="blob background shape" />
           </div>
-        </div>
+          <div className="absolute top-0 left-0 -z-10">
+            <img
+              src="/images/top-left-gradient.png"
+              alt="blob background shape"
+            />
+          </div>
+          <div className="relative">
+            <CategorySection />
+            <div className="absolute top-0 right-0">
+              <img src="/images/plus-group.png" alt="blob background shape" />
+            </div>
+          </div>
 
-        <TopSellingSection />
-        <BookNextTripSection />
-        <TestimonialSection />
-        <LogoGroupSection />
-        <NewsLetterSection />
-        <FooterSection />
-      </div>
-    </main>
+          <TopSellingSection />
+          <BookNextTripSection />
+          <TestimonialSection />
+          <LogoGroupSection />
+          <NewsLetterSection />
+          <FooterSection />
+        </div>
+      </main>
+    </>
   );
 }
