f629560fec07668f3530f869531e5a32efd3da17
Farhan Aziz <farhan@aththariq.com>
2024-02-08 16:49:25 +0700

    Add getAllPaginated endpoint to eventRegistrationRouter

--- apps/admin/src/server/api/routers/eventRegistration.ts
+++ apps/admin/src/server/api/routers/eventRegistration.ts
@@ -2,8 +2,32 @@ import { createTRPCRouter, adminProcedure } from "@/server/api/trpc";
 import { getBackendApi } from "@/lib/axios";
 import { TRPCError } from "@trpc/server";
 import { addEventRegistrationSchema } from "@repo/validators/eventRegistration";
+import { getPaginatedDataSchema } from "@repo/validators";
 
 export const eventRegistrationRouter = createTRPCRouter({
+  getAllPaginated: adminProcedure
+    .input(getPaginatedDataSchema)
+    .query(async ({ input, ctx }) => {
+      const accessToken = ctx.session.accessToken;
+      const { page, take, search } = input;
+
+      try {
+        const result = await getBackendApi(accessToken, {
+          page,
+          take,
+          search,
+        }).get("/event-registrations");
+
+        return result.data;
+      } catch (error) {
+        console.error("eventRegistrationRouter getAllPaginated", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to get events registrations",
+        });
+      }
+    }),
+
   create: adminProcedure
     .input(addEventRegistrationSchema)
     .mutation(async ({ input, ctx }) => {
