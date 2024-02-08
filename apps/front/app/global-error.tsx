ff02c6cf2c56ba39b5a11eecc2b8b395d82fad6a
Farhan Aziz <farhan@aththariq.com>
2024-02-08 22:49:30 +0700

    Update global error page layout

--- apps/front/app/global-error.tsx
+++ apps/front/app/global-error.tsx
@@ -16,23 +16,32 @@ export default function GlobalError({
   }, [error]);
 
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center space-y-10 bg-sky-50/50">
-      <Image src={'/images/error.svg'} width={600} height={600} alt="error" />
+    <html>
+      <body>
+        <main className="min-h-screen flex flex-col justify-center items-center space-y-10 bg-sky-50/50">
+          <Image
+            src={'/images/error.svg'}
+            width={600}
+            height={600}
+            alt="error"
+          />
 
-      <div className="space-y-5">
-        <h1 className="text-4xl font-bold text-center text-title">
-          Internal server error
-        </h1>
-        <p className="text-center inline-block">
-          It shouldn&apos;t have happened! We&apos;ve been notified about this{' '}
-          <br />
-          issue and we&apos;ll take a look at it shortly.
-        </p>
-      </div>
+          <div className="space-y-5">
+            <h1 className="text-4xl font-bold text-center text-title">
+              Internal server error
+            </h1>
+            <p className="text-center inline-block">
+              It shouldn&apos;t have happened! We&apos;ve been notified about
+              this <br />
+              issue and we&apos;ll take a look at it shortly.
+            </p>
+          </div>
 
-      <Link href={'/'}>
-        <MainButton text="Back to Home" size="xlarge" width="100" />
-      </Link>
-    </main>
+          <Link href={'/'}>
+            <MainButton text="Back to Home" size="xlarge" width="100" />
+          </Link>
+        </main>
+      </body>
+    </html>
   );
 }
