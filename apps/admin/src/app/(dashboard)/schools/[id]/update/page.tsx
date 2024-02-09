f24ac21404ffc7abb930b0b5efe10e351c92cb85
Farhan Aziz <farhan@aththariq.com>
2024-02-09 21:28:10 +0700

    Fix error handling in UpdateSchoolPage

--- apps/admin/src/app/(dashboard)/schools/[id]/update/page.tsx
+++ apps/admin/src/app/(dashboard)/schools/[id]/update/page.tsx
@@ -1,13 +1,21 @@
 import HeadingWithAction from "@/components/HeadingWithAction";
 import UpdateSchoolForm from "@/components/forms/UpdateSchoolForm";
 import { api } from "@/trpc/server";
+import { notFound } from "next/navigation";
 
 const UpdateSchoolPage = async ({
   params: { id },
 }: {
   params: { id: string };
 }) => {
-  const school = await api.school.getById.query({ id });
+  let school = null;
+  try {
+    school = await api.school.getById.query({ id });
+  } catch (error) {
+    console.error("UpdateSchoolPage", error);
+    notFound();
+  }
+  if (!school) notFound();
 
   return (
     <>
