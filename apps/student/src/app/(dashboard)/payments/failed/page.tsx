9d33b804a441e76ff829a34286e5db7329499de3
Farhan Aziz <farhan@aththariq.com>
2024-02-10 19:40:54 +0700

    Update styling for payment failed page

--- apps/student/src/app/(dashboard)/payments/failed/page.tsx
+++ apps/student/src/app/(dashboard)/payments/failed/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 const PaymentFailedPage = () => {
   return (
-    <main className="">
+    <main className="container py-10">
       <section className="flex min-h-[calc(100vh-8.6rem)] w-full flex-col items-center justify-center">
         <PaymentFailedLottie />
         <p className="text-3xl font-medium text-red-500">Failed</p>
-        <p className="my-3 font-medium text-primary">
+        <p className="my-3 text-center font-medium text-primary">
           Unfortunately payment was not successful
         </p>
         <p className="text-balance text-center text-muted-foreground">
