d80cbf1fe7b864688ba003d640092c0d359af5c5
Farhan Aziz <farhan@aththariq.com>
2024-02-10 13:29:30 +0700

    Update next.config.js file

---
+++ apps/student/next.config.js
@@ -0,0 +1,46 @@
+await import("./src/env.js");
+import { withSentryConfig } from "@sentry/nextjs";
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  cacheOnFrontEndNav: true,
+});
+
+/** @type {import("next").NextConfig} */
+const defaultConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*.edgestore.dev",
+        protocol: "https",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https",
+      },
+      {
+        hostname: "loremflickr.com",
+        protocol: "https",
+      },
+    ],
+  },
+};
+
+export default withSentryConfig(
+  withSerwist(defaultConfig),
+  {
+    silent: true,
+    org: "aththariq",
+    project: "tw-student",
+  },
+  {
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+  },
+);
