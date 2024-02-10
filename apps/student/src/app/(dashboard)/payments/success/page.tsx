b474e65ea44c2755e277e613bebb3409eee97403
Farhan Aziz <farhan@aththariq.com>
2024-02-10 19:41:06 +0700

    Update PaymentSuccessPage styling

--- apps/student/src/app/(dashboard)/payments/success/page.tsx
+++ apps/student/src/app/(dashboard)/payments/success/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 const PaymentSuccessPage = () => {
   return (
-    <main className="">
+    <main className="container py-10">
       <section className="flex min-h-[calc(100vh-8.6rem)] w-full flex-col items-center justify-center">
         <PaymentSuccessLottie />
         <p className="text-3xl font-medium text-green-500">Success</p>
-        <p className="my-3 font-medium text-primary">
+        <p className="my-3 text-center font-medium text-primary">
           Your payment has been processed successfully
         </p>
         <p className="text-balance text-center text-muted-foreground">
