3b39e585dafdea96bbf5ae2730200fe0f3fd0824
Farhan Aziz <farhan@aththariq.com>
2024-02-10 13:29:33 +0700

    Add purpose attribute to manifest icon

--- apps/student/src/app/manifest.ts
+++ apps/student/src/app/manifest.ts
@@ -20,6 +20,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
       },
       {
         src: "/icons/android-chrome-512x512.png",
