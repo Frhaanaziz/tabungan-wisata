ea44b929c6faac0b9097c86c9cf8dbf9be4155f5
Farhan Aziz <farhan@aththariq.com>
2024-02-12 20:56:25 +0700

    Add NotificationsPage component

---
+++ apps/student/src/app/(dashboard)/notifications/page.tsx
@@ -0,0 +1,44 @@
+import { SearchDataTable } from "@/components/data-table/SearchDataTable";
+import { notificationColumn } from "@/components/data-table/columns/NotificationColumn";
+import { api } from "@/trpc/server";
+import { Suspense } from "react";
+
+const NotificationsPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  const page = (searchParams.page as string) || "1";
+  const search = (searchParams.search as string) || "";
+
+  const data = await api.user.getNotificationsPaginated.query({
+    page: parseInt(page),
+    search,
+  });
+
+  const { content, ...utils } = data;
+
+  return (
+    <div className="container min-h-[calc(100vh-105px-112px)]">
+      <header>
+        <h1 className="text-2xl font-bold">Notifications</h1>
+        <p className="text-muted-foreground">
+          Stay up-to-date on transactions, alerts, and other updates.
+        </p>
+      </header>
+
+      <section className="mt-16">
+        <Suspense fallback={null}>
+          <SearchDataTable
+            utils={utils}
+            data={content}
+            columns={notificationColumn}
+            emptyState="No notifications."
+          />
+        </Suspense>
+      </section>
+    </div>
+  );
+};
+
+export default NotificationsPage;
