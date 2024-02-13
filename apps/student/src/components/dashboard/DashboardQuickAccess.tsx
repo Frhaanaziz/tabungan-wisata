5c78613743f394c918e9e0b49f4c2f476d6ebd8d
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:09:26 +0700

    Add DashboardQuickAccess component

---
+++ apps/student/src/components/dashboard/DashboardQuickAccess.tsx
@@ -0,0 +1,65 @@
+import { companyContact } from "@repo/utils/constants";
+import Link from "next/link";
+import TopUpButton from "../TopUpButton";
+import QuickAccessCard from "./QuickAccessCard";
+import { env } from "@/env";
+import { checkSessionAction } from "@/app/_actions";
+import { Event, EventRegistration } from "@repo/types";
+
+type Props = { eventRegistrations: (EventRegistration & { event: Event })[] };
+
+const DashboardQuickAccess = async ({ eventRegistrations }: Props) => {
+  const user = (await checkSessionAction()).data;
+
+  const event = eventRegistrations.find((registration) => registration.event);
+
+  return (
+    <>
+      <h2 className="mb-5 text-xl font-semibold">Quick Access</h2>
+      <div className="grid grid-cols-4 gap-10">
+        <TopUpButton
+          userId={user.id}
+          TriggerComponent={
+            <QuickAccessCard
+              icon={"RiExchangeFundsLine"}
+              text="Top Up"
+              tooltip="add balance to your account"
+              className="cursor-pointer"
+            />
+          }
+        />
+
+        <Link href={`https://wa.me/${companyContact}`} target="_blank">
+          <QuickAccessCard
+            icon={"RiCustomerService2Line"}
+            text="CS"
+            tooltip="customer service"
+          />
+        </Link>
+
+        {event && (
+          <Link
+            href={`${env.NEXT_PUBLIC_FRONT_URL}/events/${event.id}`}
+            target="_blank"
+          >
+            <QuickAccessCard
+              icon={"RiCalendarEventLine"}
+              text="Event"
+              tooltip="your event details"
+            />
+          </Link>
+        )}
+
+        <QuickAccessCard
+          icon={"RiLogoutBoxRLine"}
+          text="Sign Out"
+          tooltip="sign out from your account"
+          className="cursor-pointer"
+          forSignOut={true}
+        />
+      </div>
+    </>
+  );
+};
+
+export default DashboardQuickAccess;
