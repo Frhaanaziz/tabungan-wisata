6d2f59a00356c4fbe94bac72a0acd61fefbe6997
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:09:54 +0700

    Refactor paymentRouter in student API

--- apps/student/src/server/api/routers/payments.ts
+++ apps/student/src/server/api/routers/payments.ts
@@ -1,59 +1,14 @@
 import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
 import { getBackendApi } from "@/lib/axios";
 import { TRPCError } from "@trpc/server";
-import { getPaginatedDataSchema } from "@repo/validators";
 import { z } from "zod";
 import {
   addPaymentSchema,
-  paymentSchema,
   updatePaymentSchema,
 } from "@repo/validators/payment";
 import { baseUrl } from "@/lib/constant";
 
 export const paymentRouter = createTRPCRouter({
-  getAllPaginated: privateProcedure
-    .input(getPaginatedDataSchema.extend({ userId: z.string().cuid() }))
-    .query(async ({ input, ctx }) => {
-      const { userId, ...rest } = input;
-      const accessToken = ctx.session.accessToken;
-
-      try {
-        const result = await getBackendApi(accessToken, rest).get(
-          `/users/${userId}/payments`,
-        );
-
-        return result.data;
-      } catch (error) {
-        console.error("paymentRouter getAllPaginated", error);
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to get payments",
-        });
-      }
-    }),
-
-  getAll: privateProcedure
-    .input(z.object({ userId: z.string().cuid() }))
-    .output(z.array(paymentSchema))
-    .query(async ({ input, ctx }) => {
-      const { userId, ...rest } = input;
-      const accessToken = ctx.session.accessToken;
-
-      try {
-        const result = await getBackendApi(accessToken, rest).get(
-          `/users/${userId}/payments`,
-        );
-
-        return result.data;
-      } catch (error) {
-        console.error("paymentRouter getAll", error);
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to get payments",
-        });
-      }
-    }),
-
   update: privateProcedure
     .input(updatePaymentSchema)
     .mutation(async ({ input, ctx }) => {
