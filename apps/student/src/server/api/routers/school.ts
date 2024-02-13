c81965efa71b00222ac1e8ce935c1cd438fda642
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:09:57 +0700

    Add privateProcedure to schoolRouter for retrieving event registrations

--- apps/student/src/server/api/routers/school.ts
+++ apps/student/src/server/api/routers/school.ts
@@ -1,8 +1,34 @@
-import { createTRPCRouter } from "@/server/api/trpc";
-// import { getBackendApi } from "@/lib/axios";
-// import { TRPCError } from "@trpc/server";
-// import { schoolSchema } from "@repo/validators/school";
-// import { z } from "zod";
-// import { eventSchema } from "@repo/validators/event";
+import { getBackendApi } from "@/lib/axios";
+import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
+import { eventSchema } from "@repo/validators/event";
+import { eventRegistrationSchema } from "@repo/validators/eventRegistration";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
-export const schoolRouter = createTRPCRouter({});
+export const schoolRouter = createTRPCRouter({
+  getEventRegistrations: privateProcedure
+    .output(z.array(eventRegistrationSchema.extend({ event: eventSchema })))
+    .query(async ({ ctx }) => {
+      const session = ctx.session;
+      const user = ctx.session.data;
+      if (!user.schoolId)
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "Please choose a school first",
+        });
+
+      try {
+        const { data } = await getBackendApi(session.accessToken).get(
+          `/schools/${user.schoolId}/eventRegistrations`,
+        );
+
+        return data;
+      } catch (error) {
+        console.error("schoolRouter getEventRegistrations", error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to get event registrations",
+        });
+      }
+    }),
+});
