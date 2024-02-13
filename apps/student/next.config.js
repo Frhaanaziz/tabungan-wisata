341658e297d66df4daebde78539b36924432ff4d
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:08:39 +0700

    Enable service worker in production environment

--- apps/student/next.config.js
+++ apps/student/next.config.js
@@ -6,7 +6,7 @@ const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
   cacheOnFrontEndNav: true,
-  // disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import("next").NextConfig} */
