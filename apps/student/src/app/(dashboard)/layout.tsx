5d3ef6481e539c899ea548c64f773532f1384ce0
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:08:49 +0700

    Refactor DashboardLayout component

--- apps/student/src/app/(dashboard)/layout.tsx
+++ apps/student/src/app/(dashboard)/layout.tsx
@@ -1,19 +1,18 @@
 import { type ReactNode } from "react";
-import { checkSessionAction } from "../_actions";
 import Footer from "@/components/Footer";
 import { api } from "@/trpc/server";
 import Sidebar from "@/components/Sidebar";
+import { checkSessionAction } from "../_actions";
 
 const DashboardLayout = async ({ children }: { children: ReactNode }) => {
   const session = await checkSessionAction();
-  const schoolId = session.data.schoolId as string;
 
   const [eventRegistrations, highlightedEvents] = await Promise.all([
-    api.eventRegistration.getBySchoolId.query({ schoolId }),
+    api.school.getEventRegistrations.query(),
     api.event.getHighlighted.query(),
   ]);
 
-  const events = eventRegistrations.map((event) => event.event);
+  const events = eventRegistrations.map((registration) => registration.event);
 
   return (
     <>
