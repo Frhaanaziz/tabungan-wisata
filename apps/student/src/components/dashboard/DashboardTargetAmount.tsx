92f8ad25989511fa0fc6c31f2f3bf43d87db3da5
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:09:33 +0700

    Add DashboardTargetAmount component

---
+++ apps/student/src/components/dashboard/DashboardTargetAmount.tsx
@@ -0,0 +1,39 @@
+import { Card } from "@tremor/react";
+import { toRupiah } from "@repo/utils";
+import ProgressBar from "./ProgressBar";
+import { Event, EventRegistration } from "@repo/types";
+
+interface Props {
+  balance: number;
+  eventRegistrations: (EventRegistration & { event: Event })[];
+}
+
+const DashboardTargetAmount = async ({
+  balance,
+  eventRegistrations,
+}: Props) => {
+  const targetAmount = eventRegistrations.reduce(
+    (acc, registration) => acc + registration.cost,
+    0,
+  );
+  const progress = (balance / targetAmount) * 100;
+  const amountLeft = targetAmount - balance;
+
+  return (
+    <Card>
+      <div className="flex items-end justify-between">
+        <h3>Target Amount</h3>
+        <p className="text-sm">{toRupiah(amountLeft)} left</p>
+      </div>
+
+      <ProgressBar value={Number(progress.toFixed(0)) || 0} className="my-3" />
+
+      <p className="flex items-center justify-between text-sm text-muted-foreground">
+        <span>{progress.toFixed(0)}% of target amount</span>
+        <span>{toRupiah(targetAmount)}</span>
+      </p>
+    </Card>
+  );
+};
+
+export default DashboardTargetAmount;
