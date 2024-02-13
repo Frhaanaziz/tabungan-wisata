32bfd4196f012ee070f1efc01a5c5ae100318e3e
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:08:56 +0700

    Refactor dashboard page layout and components

--- apps/student/src/app/(dashboard)/page.tsx
+++ apps/student/src/app/(dashboard)/page.tsx
@@ -1,66 +1,52 @@
-import { getFirstName, greeting, toRupiah } from "@repo/utils";
-import { AspectRatio } from "@ui/components/shadcn/aspect-ratio";
-import Image from "next/image";
-import { checkSessionAction } from "../_actions";
+import DashboardAreaChart from "@/components/dashboard/DashboardAreaChart";
+import DashboardEventTimeout from "@/components/dashboard/DashboardEventTimeout";
+import DashboardQuickAccess from "@/components/dashboard/DashboardQuickAccess";
+import DashboardRecentTransactions from "@/components/dashboard/DashboardRecentTransactions";
+import DashboardTargetAmount from "@/components/dashboard/DashboardTargetAmount";
 import { api } from "@/trpc/server";
-import TopUpButton from "@/components/TopUpButton";
-import { redirect } from "next/navigation";
-import { DataTable } from "@ui/components/table/data-table";
-import { paymentColumn } from "@/components/data-table/columns/PaymentColumn";
+import Link from "next/link";
+
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const { data: user } = await checkSessionAction();
-  if (!user.schoolId) redirect("/auth/school");
-
-  const [userPayments, userBalance, school] = await Promise.all([
-    api.payment.getAll.query({ userId: user.id }),
+  const [balance, eventRegistrations] = await Promise.all([
     api.user.getBalance.query(),
-    api.eventRegistration.getBySchoolId.query({ schoolId: user.schoolId }),
+    api.school.getEventRegistrations.query(),
   ]);
-  const eventsCost = school
-    .map(({ event }) => event.cost)
-    .reduce((a, b) => a + b, 0);
 
   return (
-    <main className="container">
-      <section className="grid-cols-5 items-center space-y-10 md:grid md:space-y-0">
-        <div className="order-2 col-span-2">
-          <AspectRatio ratio={3 / 2}>
-            <Image
-              src={"/images/frugality.jpg"}
-              alt="frugality"
-              priority
-              fill
-              sizes="50vw"
-            />
-          </AspectRatio>
+    <div className="container grid min-h-[calc(100vh-105px-112px)] max-w-[1600px] grid-cols-5 gap-10">
+      <section className="col-span-2 space-y-10">
+        <DashboardQuickAccess eventRegistrations={eventRegistrations} />
+
+        <div className="space-y-2">
+          <h3>Your balance</h3>
+          <div className="flex items-end justify-between">
+            <p className="text-4xl font-semibold">
+              {balance.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}
+            </p>
+            <Link href={"/transactions"} className="block text-sm text-primary">
+              View Transactions
+            </Link>
+          </div>
         </div>
 
-        <div className="col-span-3">
-          <h1 className="text-4xl font-bold md:text-5xl">
-            Good {greeting()} {getFirstName(user.name).toLowerCase()},
-            <br />
-            you have
-            <br />
-          </h1>
-          <p className="my-5 text-4xl font-bold text-green-500">
-            {toRupiah(userBalance)}
-          </p>
-          <p className="mb-4 text-lg">
-            Your target balance to participate in the tour:{" "}
-            <span className="underline">{toRupiah(eventsCost)}</span>
-          </p>
-          <TopUpButton userId={user.id} />
-        </div>
-      </section>
-
-      <section className="mt-20">
-        <DataTable
-          columns={paymentColumn as any}
-          data={userPayments}
-          emptyMessage={"No transactions."}
+        <DashboardTargetAmount
+          balance={balance}
+          eventRegistrations={eventRegistrations}
         />
+
+        <DashboardEventTimeout eventRegistrations={eventRegistrations} />
       </section>
-    </main>
+
+      <section className="col-span-3 space-y-10">
+        <DashboardAreaChart balance={balance} />
+
+        <DashboardRecentTransactions />
+      </section>
+    </div>
   );
 }
