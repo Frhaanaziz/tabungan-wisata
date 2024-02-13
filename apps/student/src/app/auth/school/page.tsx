ad40ee40c1e4a28d3ae111e52e7b5ac6eb3c7b5e
Farhan Aziz <farhan@aththariq.com>
2024-02-13 21:09:03 +0700

    Add checkSessionAction and notFound function to AuthSchoolPage

--- apps/student/src/app/auth/school/page.tsx
+++ apps/student/src/app/auth/school/page.tsx
@@ -1,6 +1,11 @@
+import { checkSessionAction } from "@/app/_actions";
 import AddSchoolForm from "@/components/forms/AddSchoolForm";
+import { notFound } from "next/navigation";
 
-const AuthSchoolPage = () => {
+const AuthSchoolPage = async () => {
+  const user = (await checkSessionAction()).data;
+  if (user.schoolId) notFound();
+
   return (
     <main className="flex min-h-screen flex-col justify-center  bg-muted">
       <section className="mx-6 border bg-card px-6 py-12 shadow sm:mx-auto sm:w-full sm:max-w-[480px] sm:rounded-lg sm:px-12">
