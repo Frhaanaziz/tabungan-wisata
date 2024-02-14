19952d05d854d444c6be0d7f5ece937b7791de53
Farhan Aziz <farhan@aththariq.com>
2024-02-14 10:01:45 +0700

    Add initial balance to DashboardAreaChart

--- apps/student/src/components/dashboard/DashboardAreaChart.tsx
+++ apps/student/src/components/dashboard/DashboardAreaChart.tsx
@@ -2,16 +2,24 @@ import { api } from "@/trpc/server";
 import BalanceAreaChart from "./BalanceAreaChart";
 import { PaymentStatus } from "@repo/validators/payment";
 import { formatDateToShortMonthDay } from "@repo/utils";
+import { checkSessionAction } from "@/app/_actions";
 
 const DashboardAreaChart = async ({ balance }: { balance: number }) => {
+  const user = (await checkSessionAction()).data;
+
   const [payments] = await Promise.all([
     api.user.getAllPayments.query({
       status: PaymentStatus.completed,
     }),
   ]);
 
+  const withInitialBalance = [
+    { createdAt: user.createdAt, amount: 0 },
+    ...payments,
+  ];
+
   let balanceAmnt = 0;
-  const chartData = payments.map(({ createdAt, amount }) => {
+  const chartData = withInitialBalance.map(({ createdAt, amount }) => {
     balanceAmnt += amount;
     return {
       date: formatDateToShortMonthDay(createdAt),
