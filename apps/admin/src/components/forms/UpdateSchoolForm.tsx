bde277627bc6fe21f7a55b421d4448b87a9ae9ba
Farhan Aziz <farhan@aththariq.com>
2024-02-17 23:31:14 +0700

    Update UpdateSchoolForm component to include useFieldArray hook

--- apps/admin/src/components/forms/UpdateSchoolForm.tsx
+++ apps/admin/src/components/forms/UpdateSchoolForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import type * as z from "zod";
 
 import {
@@ -18,17 +18,30 @@ import { Textarea } from "@ui/components/shadcn/textarea";
 import SubmitButton from "../SubmitButton";
 import { api } from "@/trpc/react";
 import { toast } from "sonner";
+import FormFieldWrapper from "./FormFieldWrapper";
+import { Button } from "@ui/components/shadcn/button";
+import { XIcon } from "lucide-react";
 
 type UpdateSchoolType = z.infer<typeof updateSchoolSchema>;
 
 const UpdateSchoolForm = ({ school }: { school: UpdateSchoolType }) => {
   const utils = api.useUtils();
+
   const form = useForm<UpdateSchoolType>({
     resolver: zodResolver(updateSchoolSchema),
     defaultValues: school,
   });
 
   const { handleSubmit, control } = form;
+
+  const {
+    fields,
+    append: addPIC,
+    remove: removePIC,
+  } = useFieldArray({
+    name: "schoolAdmins",
+    control,
+  });
 
   const { mutate, isLoading } = api.school.update.useMutation({
     onSuccess: async () => {
@@ -89,6 +102,70 @@ const UpdateSchoolForm = ({ school }: { school: UpdateSchoolType }) => {
               </FormItem>
             )}
           />
+
+          {fields.map((field, i) => (
+            <FormFieldWrapper key={field.id}>
+              <FormField
+                control={form.control}
+                key={field.id}
+                name={`schoolAdmins.${i}.name`}
+                disabled={isLoading}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="space-x-2">
+                      <span>School PIC #{i + 1} name</span>
+                      {(!!i || fields.length > 1) && (
+                        <Button
+                          type="button"
+                          size={"icon"}
+                          className="h-4 w-4"
+                          disabled={isLoading}
+                          onClick={() => removePIC(i)}
+                        >
+                          <XIcon className="h-3 w-3" />
+                        </Button>
+                      )}
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder={`Enter school PIC #${i + 1} name`}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                key={field.id}
+                name={`schoolAdmins.${i}.contact`}
+                disabled={isLoading}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel> School PIC #{i + 1} contact</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder={`Enter school PIC #${i + 1} contact`}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </FormFieldWrapper>
+          ))}
+          <Button
+            type="button"
+            variant="outline"
+            size="sm"
+            className="mt-2"
+            disabled={isLoading}
+            onClick={() => addPIC({ name: "", contact: "" })}
+          >
+            Add PIC
+          </Button>
         </div>
 
         <SubmitButton isSubmitting={isLoading} />
