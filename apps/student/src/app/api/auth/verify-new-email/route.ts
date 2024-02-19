fe0a0bec833004f14b46d9119fe3c5aaf77b5b6e
Farhan Aziz <farhan@aththariq.com>
2024-02-19 23:34:50 +0700

    Add route for verifying new email

---
+++ apps/student/src/app/api/auth/verify-new-email/route.ts
@@ -0,0 +1,27 @@
+import { getNestErrorMessage } from "@repo/utils";
+import { NextResponse } from "next/server";
+import { getBackendApi } from "@/lib/axios";
+import { baseUrl } from "@/lib/constant";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const token = searchParams.get("token");
+  const userId = searchParams.get("id");
+  if (typeof token !== "string" || typeof userId !== "string")
+    return NextResponse.json(
+      { error: "Token or ID not found" },
+      { status: 500 },
+    );
+
+  try {
+    const data = { emailVerified: true, token };
+    await getBackendApi(token).patch(`/users/${userId}/emailVerified`, data);
+
+    return NextResponse.redirect(baseUrl);
+  } catch (error) {
+    return NextResponse.json(
+      { error: getNestErrorMessage(error) },
+      { status: 500 },
+    );
+  }
+}
