23b601974ea4377d92f52d210defce7cfe1f1813
Farhan Aziz <farhan@aththariq.com>
2024-02-19 23:35:47 +0700

    Refactor user validation schemas

--- packages/validators/user.ts
+++ packages/validators/user.ts
@@ -8,9 +8,19 @@ export enum UserRole {
 
 export const userSchema = z.object({
   id: z.string().cuid(),
-  name: z.string().min(1).max(190),
-  email: z.string().email().max(190),
-  password: z.string().min(6).max(190).nullable(),
+  name: z
+    .string()
+    .min(1, { message: 'Name must be at least 1 character long' })
+    .max(190, { message: 'Name must not exceed 190 characters' }),
+  email: z
+    .string()
+    .email({ message: 'Invalid email format' })
+    .max(190, { message: 'Email must not exceed 190 characters' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' })
+    .max(190, { message: 'Password must not exceed 190 characters' })
+    .nullable(),
   role: z.nativeEnum(UserRole),
   balance: z.number().int().min(0),
   schoolId: z.string().cuid().nullable(),
@@ -26,3 +36,39 @@ export const updateUserSchema = userSchema.omit({
   createdAt: true,
   updatedAt: true,
 });
+
+export const updateUserPasswordSchema = z
+  .object({
+    currentPassword: z
+      .string()
+      .min(6, {
+        message: 'Current password must be at least 6 characters long',
+      })
+      .max(190, { message: 'Current password must not exceed 190 characters' }),
+    newPassword: z
+      .string()
+      .min(6, { message: 'New password must be at least 6 characters long' })
+      .max(190, { message: 'New password must not exceed 190 characters' }),
+    confirmNewPassword: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.newPassword === data.confirmNewPassword;
+    },
+    { message: 'Passwords do not match', path: ['confirmNewPassword'] }
+  );
+
+export const createUserPasswordSchema = z
+  .object({
+    newPassword: z
+      .string()
+      .min(6, { message: 'New password must be at least 6 characters long' })
+      .max(190, { message: 'New password must not exceed 190 characters' }),
+    confirmNewPassword: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.newPassword === data.confirmNewPassword;
+    },
+    { message: 'Passwords do not match', path: ['confirmNewPassword'] }
+  );
