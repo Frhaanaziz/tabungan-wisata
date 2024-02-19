2ac7b2ebfd6ae5330f1b6b4bbff16d7a00580704
Farhan Aziz <farhan@aththariq.com>
2024-02-19 23:35:44 +0700

    Add image validation to validators package

--- packages/validators/index.ts
+++ packages/validators/index.ts
@@ -16,3 +16,13 @@ export const getPaginatedDataSchema = z.object({
   take: z.coerce.number().min(1).optional(),
   search: z.string().optional().default(''),
 });
+
+const acceptImageTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+export const imageSchema = z
+  .instanceof(File)
+  .refine((file) => file.size < 1 * 1024 * 1024, {
+    message: 'File size must be less than 1MB',
+  })
+  .refine((file) => acceptImageTypes.includes(file.type), {
+    message: 'File type must be PNG, JPEG, or JPG',
+  });
