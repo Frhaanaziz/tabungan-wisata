bd245cb5cfc71bf9e2ba1ed927f255a166617660
Farhan Aziz <farhan@aththariq.com>
2024-03-07 21:48:40 +0700

    Refactor event registration schema and add validation for payment limit

--- packages/validators/eventRegistration.ts
+++ packages/validators/eventRegistration.ts
@@ -1,21 +1,33 @@
 import { z } from 'zod';
 
-export const eventRegistrationSchema = z.object({
+export const baseEventRegistrationSchema = z.object({
   id: z.string().cuid(),
-  eventId: z.string().cuid({ message: 'Please select an event' }),
-  schoolId: z.string().cuid(),
   cost: z.coerce.number().min(1, { message: 'Cost must be at least 1' }),
+  paymentLimit: z.coerce.date(),
   startDate: z.coerce.date(),
   endDate: z.coerce.date(),
+  eventId: z.string().cuid({ message: 'Please select an event' }),
+  schoolId: z.string().cuid({ message: 'Please select a school' }),
   createdAt: z.coerce.date(),
 });
 
-export const addEventRegistrationSchema = eventRegistrationSchema.omit({
+// payment limit must be before startDate
+export const eventRegistrationSchema = baseEventRegistrationSchema.refine(
+  (data) => {
+    return data.paymentLimit < data.startDate;
+  },
+  {
+    message: 'Payment limit must be before start date',
+    path: ['paymentLimit'],
+  }
+);
+
+export const addEventRegistrationSchema = baseEventRegistrationSchema.omit({
   id: true,
   createdAt: true,
 });
 
-export const updateEventRegistrationSchema = eventRegistrationSchema.omit({
+export const updateEventRegistrationSchema = baseEventRegistrationSchema.omit({
   createdAt: true,
   schoolId: true,
 });
