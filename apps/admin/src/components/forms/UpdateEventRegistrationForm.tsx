d237152a2f5eca076f92d1de2c486d6742320662
Farhan Aziz <farhan@aththariq.com>
2024-03-07 21:48:21 +0700

    Update UpdateEventRegistrationForm component

--- apps/admin/src/components/forms/UpdateEventRegistrationForm.tsx
+++ apps/admin/src/components/forms/UpdateEventRegistrationForm.tsx
@@ -157,9 +157,45 @@ const UpdateEventRegistrationForm = ({
                       selected={field.value}
                       onSelect={field.onChange}
                       defaultMonth={field.value}
-                      // disabled={(date) =>
-                      //   date > new Date() || date < new Date("1900-01-01")
-                      // }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="paymentLimit"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Payment limit</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground",
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
                       initialFocus
                     />
                   </PopoverContent>
